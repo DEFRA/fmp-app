854a67bf43e21fec627aacba91a977f09d987be5
Paul Shaw <pmshaw.ea@gmail.com>
2016-11-04 12:22:21 +0000

    FLO-1718 add stub address search option

---
+++ server/mock/address/find-by-place/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'BT45': require('./BT45.json'),
+  'CREGGAN': require('./CREGGAN.json'),
+  'EH1': require('./EH1.json'),
+  'GLASGOW': require('./GLASGOW.json'),
+  'L129ES': require('./L129ES.json'),
+  'LLANELLI': require('./LLANELLI.json'),
+  'SY232HN': require('./SY232HN.json'),
+  'WA41AG': require('./WA41AG.json'),
+  'WA41HT': require('./WA41HT.json'),
+  'WA51DJ': require('./WA51DJ.json')
+}
