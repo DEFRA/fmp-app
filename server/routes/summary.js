207c128f5db4cb3b531cc45d37826f5e194f8614
EADEV <davidjamesstone@gmail.com>
2018-02-05 14:03:58 +0000

    Confirm location

--- server/routes/summary.js
+++ server/routes/summary.js
@@ -7,17 +7,29 @@ module.exports = {
   method: 'GET',
   path: '/summary/{easting}/{northing}',
   options: {
-    description: 'Get flood risk summary',
+    description: 'Get flood risk summary for a point',
     handler: async (request, h) => {
       try {
         const easting = encodeURIComponent(request.params.easting)
         const northing = encodeURIComponent(request.params.northing)
-        const result = await riskService.get(easting, northing)
-        if (!result.point_in_england) {
-          return h.view('not-england')
+        // const polygon = request.query.polygon
+
+        if (request.query.polygon) {
+          const result = await riskService.getByPoint(easting, northing)
+          if (!result.point_in_england) {
+            return h.view('not-england')
+          } else {
+            return h.view('summary', new SummaryViewModel(easting, northing, result))
+              .unstate('pdf-download')
+          }
         } else {
-          return h.view('summary', new SummaryViewModel(easting, northing, result))
-            .unstate('pdf-download')
+          const result = await riskService.getByPoint(easting, northing)
+          if (!result.point_in_england) {
+            return h.view('not-england')
+          } else {
+            return h.view('summary', new SummaryViewModel(easting, northing, result))
+              .unstate('pdf-download')
+          }
         }
       } catch (err) {
         return Boom.badImplementation(err.message, err)
@@ -27,7 +39,37 @@ module.exports = {
       params: {
         easting: Joi.number().max(700000).positive().required(),
         northing: Joi.number().max(1300000).positive().required()
+      },
+      query: {
+        polygon: Joi.array().items(Joi.array().items(
+          Joi.number().max(700000).positive().required(),
+          Joi.number().max(1300000).positive().required()
+        ))
       }
     }
   }
 }
+// , {
+//   method: 'POST',
+//   path: '/summary',
+//   options: {
+//     description: 'Get flood risk summary for a polygon',
+//     handler: async (request, h) => {
+//       try {
+//         const polygon = request.payload
+//         const result = await riskService.getByPolygon(polygon)
+
+//         if (!result.point_in_england) {
+//           return h.view('not-england')
+//         } else {
+//           return h.view('summary', new SummaryViewModel(polygon, result))
+//             .unstate('pdf-download')
+//         }
+//       } catch (err) {
+//         return Boom.badImplementation(err.message, err)
+//       }
+//     },
+//     validate: {
+//     }
+//   }
+// }]
--- server/services/risk.js
+++ server/services/risk.js
@@ -3,10 +3,18 @@ const config = require('../../config')
 const url = config.service + '/zones/'
 
 module.exports = {
-  get: (easting, northing) => {
+  getByPoint: (easting, northing) => {
     if (!easting || !northing) {
       throw new Error('No Point provided')
     }
+
     return util.getJson(url + easting + '/' + northing + '/1')
+  },
+  getByPolygon: (polygon) => {
+    if (!polygon) {
+      throw new Error('No Polygon provided')
+    }
+
+    return util.postJson(url, polygon)
   }
 }
