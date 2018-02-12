7e0c6b025b66112b1d2a1e2c6e3e0957e0700e91
Tedd Mason <teddmason@gmail.com>
2018-02-12 11:51:01 +0000

    Polygon flood zone query change

--- server/models/summary-view.js
+++ server/models/summary-view.js
@@ -12,19 +12,35 @@ function SummaryView (risk, center, polygon) {
   this.isZone2 = false
   this.isZone1 = false
 
-  // Set the points flood zone
-  if (risk.areas_benefiting && risk.floodzone_3) {
-    this.isAreaBenefiting = true
-    this.zone = 'FZ3a'
-  } else if (risk.floodzone_3) {
-    this.isZone3 = true
-    this.zone = 'FZ3'
-  } else if (risk.floodzone_2) {
-    this.isZone2 = true
-    this.zone = 'FZ2'
-  } else {
-    this.isZone1 = true
-    this.zone = 'FZ1'
+  // Set the points flood zone (centroid)
+  if (!polygon) {
+    if (risk.areas_benefiting && risk.floodzone_3) {
+      this.isAreaBenefiting = true
+      this.zone = 'FZ3a'
+    } else if (risk.floodzone_3) {
+      this.isZone3 = true
+      this.zone = 'FZ3'
+    } else if (risk.floodzone_2) {
+      this.isZone2 = true
+      this.zone = 'FZ2'
+    } else {
+      this.isZone1 = true
+      this.zone = 'FZ1'
+    }
+  } else { // Set points for flood zone (polygon)
+    if (risk.floodzone_3) {
+      this.isZone3 = true
+      this.zone = 'FZ3'
+    } else if (risk.areas_benefiting) {
+      this.isAreaBenefiting = true
+      this.zone = 'FZ3a'
+    } else if (risk.floodzone_2) {
+      this.isZone2 = true
+      this.zone = 'FZ2'
+    } else {
+      this.isZone1 = true
+      this.zone = 'FZ1'
+    }
   }
 
   // Provide test data for e2e tests
