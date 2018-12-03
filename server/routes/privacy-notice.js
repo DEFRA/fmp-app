49aebd3445c5633ed63e0710a14d5e365fcb929d
nwycherley <niki.wycherley@environment-agency.gov.uk>
2018-12-03 09:18:13 +0000

    made changes from pull request feedback, added new line and removed feedback var

--- server/routes/privacy-notice.js
+++ server/routes/privacy-notice.js
@@ -11,8 +11,7 @@ module.exports = {
         context: {
           pageTitle: 'Privacy notice - Flood information service - GOV.UK',
           metaDescription: description,
-          ogDescription: description,
-          feedback: false
+          ogDescription: description
         }
       }
     }
