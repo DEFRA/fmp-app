26134641a46e30f8edf225c6f8d8fc442afbdba7
hari-gillala <Hari.Gillala@environment-agency.gov.uk>
2020-07-14 00:52:29 +0100

    Based on the correct parameters passed , page is successfully transferred to confirm location page
    Signed-off-by: hari-gillala <Hari.Gillala@environment-agency.gov.uk>

--- server/routes/location.js
+++ server/routes/location.js
@@ -31,55 +31,98 @@ module.exports = [{
     }
   },
   handler: async (request, h) => {
-    const payload = request.payload
-    // attempt to get BNG coordinates for the place or postcode
-    let BNG = {}
-    var selectedOption = payload.findby
-    if (selectedOption === 'placeOrPostcode') {
-      try {
-        if (payload.placeOrPostcode === '') {
+    try {
+      const queryParams = {}
+      let BNG = {}
+      let model = {}
+
+      const payload = request.payload
+
+      // Find the Selected Option
+      const selectedOption = payload.findby
+
+      // If its Place or Postcode
+      if (selectedOption === 'placeOrPostcode') {
+        if (payload.placeOrPostcode !== '') {
+          const address = await addressService.findByPlace(payload.placeOrPostcode)
+          if (!address || !address.length || !address[0].geometry_x || !address[0].geometry_y) {
+            throw new Error('No address found')
+          }
+          const addr = address[0]
+          BNG.easting = addr.geometry_x
+          BNG.northing = addr.geometry_y
+        } else {
           const errors = [{ text: 'Place Or Postcode is Required', href: '#placeOrPostcode' }]
-          var model = new LocationViewModel({
+          model = new LocationViewModel({
             errorSummary: errors
           })
           return h.view('location', model)
         }
-        const address = await addressService.findByPlace(payload.placeOrPostcode)
-        if (!address || !address.length || !address[0].geometry_x || !address[0].geometry_y) {
-          throw new Error('No address found')
-        }
-        const addr = address[0]
-        BNG.easting = addr.geometry_x
-        BNG.northing = addr.geometry_y
-      } catch (err) {
-        // return error view if E/N lookup by place or postcode fails
-        request.log(['error', 'address-service', 'find-by-place'], err)
-        const data = {
-          type: 'placeOrPostcode',
-          placeOrPostcode: selectedOption
-        }
-        const errors = { placeOrPostcode: 'Place or PostCode Not found' }
-        var errorSummary = Object.values(errors)
-        return h.view('location', new LocationViewModel(data, errorSummary))
       }
-    } else if (selectedOption === 'nationalGridReference') {
-      BNG = ngrToBng.convert(payload.nationalGridReference)
-    } else if (selectedOption === 'eastingNorthing') {
-      BNG.easting = payload.easting
-      BNG.northing = payload.northing
+
+      // If NGR Reference
+      else if (selectedOption === 'nationalGridReference') {
+        if (payload.nationalGridReference !== '') {
+          BNG = ngrToBng.convert(payload.nationalGridReference)
+        } else {
+          const errors = [{ text: 'National Grid Reference is Requered', href: '#nationalGridReference' }]
+          model = {}
+          model = new LocationViewModel({
+            errorSummary: errors
+          })
+          return h.view('location', model)
+        }
+      }
+
+      // If Easting and Northing
+      else if (selectedOption === 'eastingNorthing') {
+        if (payload.easting !== '' && payload.northing !== '') {
+          BNG.easting = payload.easting
+          BNG.northing = payload.northing
+        } else if (payload.easting === '' && payload.northing === '') {
+          const errors = [
+            { text: 'Easting is Required', href: '#easting' },
+            { text: 'Northing is Required', href: '#northing' }
+          ]
+          model = {}
+          model = new LocationViewModel({
+            errorSummary: errors
+          })
+          return h.view('location', model)
+        } else if (payload.easting === '') {
+          const errors = [
+            { text: 'Easting is Required', href: '#easting' }
+          ]
+          model = {}
+          model = new LocationViewModel({ errorSummary: errors })
+          return h.view('location', model)
+        } else if (payload.northing === '') {
+          const errors = [
+            { text: 'Northing is Required', href: '#northing' }
+          ]
+          model = {}
+          model = new LocationViewModel({ errorSummary: errors })
+          return h.view('location', model)
+        } else {
+
+        }
+      }
+
+      queryParams.easting = BNG.easting || ''
+      queryParams.northing = BNG.northing || ''
+      // if the search wasn't by E/N include the original search in the query params
+      if (selectedOption === 'nationalGridReference') {
+        queryParams.nationalGridReference = payload.nationalGridReference
+      } else if (selectedOption === 'placeOrPostcode') {
+        queryParams.placeOrPostcode = payload.placeOrPostcode
+      }
+
+        const query = QueryString.stringify(queryParams)
+        return h.redirect(`/confirm-location?${query}`)
+      }
+     catch (error) {
+
     }
-    // redirect to the confirm location page with the BNG in the query
-    const queryParams = {}
-    queryParams.easting = BNG.easting || ''
-    queryParams.northing = BNG.northing || ''
-    // if the search wasn't by E/N include the original search in the query params
-    if (payload.type === 'nationalGridReference') {
-      queryParams.nationalGridReference = payload.nationalGridReference
-    } else if (payload.type === 'placeOrPostcode') {
-      queryParams.placeOrPostcode = payload.placeOrPostcode
-    }
-    const query = QueryString.stringify(queryParams)
-    return h.redirect(`/confirm-location?${query}`)
   }
 }
 ]
