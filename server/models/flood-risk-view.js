5ea6a14fb9f7bb044c42ae4ee8090e03f920aa02
Harsh Vasudev <hvasudev@gmail.com>
2020-08-18 17:48:16 +0100

    FCRM-1527 flood zone results map layers

--- server/models/flood-risk-view.js
+++ server/models/flood-risk-view.js
@@ -1,12 +1,26 @@
-function FloodRiskLocationViewModel (data) {
-  if (data.psoEmailAddress) {
-    this.psoEmailAddress = data.psoEmailAddress
+const FloodZone = require('./flood-zone')
+
+function FloodRiskLocationViewModel (psoEmailAddress, areaName, zone, risk, center, polygon) {
+  if (psoEmailAddress) {
+    this.psoEmailAddress = psoEmailAddress
   }
-  if (data.areaName) {
-    this.areaName = data.areaName
+  if (areaName) {
+    this.areaName = areaName
   }
-  if (data.zone) {
-    this.zone = data.zone
+  if (zone) {
+    this.zone = zone
   }
+  if (polygon) {
+    this.polygon = JSON.stringify(polygon)
+    this.center = JSON.stringify(center)
+  }
+
+  this.easting = center[0]
+  this.northing = center[1]
+
+  this.floodZone = new FloodZone(risk, !!polygon)
+
+  // Provide test data for e2e tests
+  this.riskJSON = JSON.stringify(risk)
 }
 module.exports = FloodRiskLocationViewModel
--- server/routes/flood-zone-results.js
+++ server/routes/flood-zone-results.js
@@ -1,42 +1,85 @@
+const Boom = require('boom')
+const Joi = require('joi')
+const riskService = require('../services/risk')
+const util = require('../util')
 const FloodRiskLocationViewModel = require('../models/flood-risk-view')
 const psoContactDetails = require('../services/pso-contact')
-module.exports = [{
-  method: 'GET',
-  path: '/flood-zone-results',
-  options: {
-    description: 'Displays Flood Zone Three Results Page',
-    auth: {
-      strategy: 'restricted'
-    },
-    handler: async (request, h) => {
-      const result = await psoContactDetails.getPsoContacts(383819, 398052)
-      let psoEmailAddress = ''
-      let areaName = ''
-      if (result && result.EmailAddress) {
-        psoEmailAddress = result.EmailAddress
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/flood-zone-results',
+    options: {
+      description: 'Displays flood zone results page',
+      auth: {
+        strategy: 'restricted'
+      },
+      handler: async (request, h) => {
+        try {
+          const easting = encodeURIComponent(request.query.easting)
+          const northing = encodeURIComponent(request.query.northing)
+
+          const psoResults = await psoContactDetails.getPsoContacts(easting, northing)
+          let psoEmailAddress = ''
+          let areaName = ''
+          const zone = 3
+          if (psoResults && psoResults.EmailAddress) {
+            psoEmailAddress = psoResults.EmailAddress
+          }
+          if (psoResults && psoResults.AreaName) {
+            areaName = psoResults.AreaName
+          }
+
+          if (request.query.polygon) {
+            const center = request.query.center
+            const polygon = request.query.polygon
+            const geoJson = util.convertToGeoJson(polygon)
+
+            const result = await riskService.getByPolygon(geoJson)
+
+            if (!result.in_england) {
+              return h.redirect(`/not-england?centroid=true&easting=${center[0]}&northing=${center[1]}`)
+            } else {
+              return h.view('flood-zone-results', new FloodRiskLocationViewModel(psoEmailAddress, areaName, zone, result, center, polygon))
+                .unstate('pdf-download')
+            }
+          } else {
+            const result = await riskService.getByPoint(easting, northing)
+            if (!result.point_in_england) {
+              return h.redirect(`/not-england?easting=${easting}&northing=${northing}`)
+            } else {
+              return h.view('flood-zone-results', new FloodRiskLocationViewModel(psoEmailAddress, areaName, zone, result, [easting, northing]))
+                .unstate('pdf-download')
+            }
+          }
+        } catch (err) {
+          return Boom.badImplementation(err.message, err)
+        }
+      },
+      validate: {
+        query: Joi.alternatives().required().try([{
+          easting: Joi.number().max(700000).positive().required(),
+          northing: Joi.number().max(1300000).positive().required()
+        }, {
+          polygon: Joi.array().required().items(Joi.array().items(
+            Joi.number().max(700000).positive().required(),
+            Joi.number().max(1300000).positive().required()
+          )),
+          center: Joi.array().required()
+        }])
       }
-      if (result && result.AreaName) {
-        areaName = result.AreaName
-      }
-      var model = new FloodRiskLocationViewModel({
-        psoEmailAddress: psoEmailAddress,
-        zone: 3,
-        areaName: areaName
-      })
-      return h.view('flood-zone-results', model)
-    }
-  }
-},
-{
-  method: 'POST',
-  path: '/flood-zone-results',
-  options: {
-    description: 'Displays Flood Zone Three Results Page',
-    auth: {
-      strategy: 'restricted'
     }
   },
-  handler: async (request, h) => {
-    return h.redirect('/contact')
-  }
-}]
+  {
+    method: 'POST',
+    path: '/flood-zone-results',
+    options: {
+      description: 'Displays Flood Zone Three Results Page',
+      auth: {
+        strategy: 'restricted'
+      }
+    },
+    handler: async (request, h) => {
+      return h.redirect('/contact')
+    }
+  }]
