b85f6c16c3d351e540c28b853cf0e08afbfb1a2a
hari-gillala <Hari.Gillala@environment-agency.gov.uk>
2020-08-31 22:15:45 +0100

    Flood Zone Expanded based on Zones

--- server/models/flood-risk-expanded-view.js
+++ server/models/flood-risk-expanded-view.js
@@ -1,6 +1,8 @@
 
-function FloodRiskExpandedViewModel (easting, northing) {
+function FloodRiskExpandedViewModel (easting, northing, zone, localAuthorities) {
   this.easting = easting
   this.northing = northing
+  this.zone = zone
+  this.localAuthorities = localAuthorities
 }
 module.exports = FloodRiskExpandedViewModel
