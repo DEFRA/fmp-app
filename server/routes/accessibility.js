054c6efdfebd43de2932302e2f7bb76e1d873c52
hari <hari-gillala@environment-agency.gov.uk>
2020-09-06 22:59:16 +0100

    Added the Accessibility Statement

---
+++ server/routes/accessibility.js
@@ -0,0 +1,17 @@
+module.exports = {
+    method: 'GET',
+    path: '/accessibility',
+    options: {
+      description: 'Accessibility - Flood map for planning - GOV.UK',
+      handler: {
+        view: {
+          template: 'accessibility',
+          context: {
+            pageTitle: 'Accessibility - Flood map for planning - GOV.UK',
+            heading: 'Flood map for planning',
+            metaDescription: 'The Environment Agency Accessibility Statement'
+          }
+        }
+      }
+    }
+  }
