a9f38d2ceac62f69555caeee6c473163030cfc78
hari-gillala <hari-gillala@environment-agency.gov.uk>
2021-03-07 22:05:29 +0000

    Cookies Added

--- server/routes/cookies.js
+++ server/routes/cookies.js
@@ -1,10 +1,26 @@
+const CookieViewModel = require('../models/cookie-view')
 module.exports = [{
   method: 'GET',
   path: '/cookies',
   options: {
     description: 'Cookies Page',
     handler: async (request, h) => {
-      return h.view('cookies')
+      var errors = []
+      var model = new CookieViewModel({
+        errorSummary: errors
+      })
+      var cookie = request.state
+      if (cookie !== null && cookie.GA !== null && cookie.GA === 'Accept') {
+        model.isYesChecked = true
+        model.isNoChecked = false
+      } else if (cookie !== null && cookie.GA !== null && cookie.GA === 'Reject') {
+        model.isNoChecked = true
+        model.isYesChecked = false
+      } else {
+        model.isNoChecked = false
+        model.isYesChecked = false
+      }
+      return h.view('cookies', model)
     }
   }
 },
@@ -15,6 +31,25 @@ module.exports = [{
     description: 'Cookies'
   },
   handler: async (request, h) => {
-    return h.redirect('/cookies')
+    var updateCookie = request.state
+    var errors = []
+    var model = new CookieViewModel({
+      errorSummary: errors
+    })
+    if (request !== null && request.payload !== null & request.payload.cookie_consent !== null) {
+      if (request.payload.cookie_consent === 'yes') {
+        if (updateCookie) {
+          updateCookie.GA = 'Accept'
+          model.isNoChecked = false
+          model.isYesChecked = true
+        }
+      }
+      if (request.payload.cookie_consent === 'no') {
+        updateCookie.GA = 'Reject'
+        model.isNoChecked = true
+        model.isYesChecked = false
+      }
+    }
+    return h.redirect('/cookies', model)
   }
 }]
