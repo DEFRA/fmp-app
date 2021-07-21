7c3879e0955812cb9b05bcbfc8c0297993b03549
hari-gillala <hari-gillala@environment-agency.gov.uk>
2021-07-21 21:50:04 +0100

    Added the new parameter required for the EmailConfirmation V2

--- server/services/email-confirmation.js
+++ server/services/email-confirmation.js
@@ -2,12 +2,12 @@ const config = require('../../config')
 const Wreck = require('wreck')
 const url = config.functionAppUrl + '/email/confirmation'
 
-async function emailConfirmation (fullname, referencenumber, location, areaname, psoemailaddress, recipientemail) {
+async function emailConfirmation (fullname, referencenumber, location, areaname, psoemailaddress, recipientemail, search) {
   try {
     if (!location) {
       throw new Error('No point provided')
     }
-    const data = JSON.stringify({ fullname: fullname, referencenumber: referencenumber, areaname: areaname, psoemailaddress: psoemailaddress, recipientemail: recipientemail, location: location })
+    const data = JSON.stringify({ fullname: fullname, referencenumber: referencenumber, areaname: areaname, psoemailaddress: psoemailaddress, recipientemail: recipientemail, location: location, search: search })
     await Wreck.post(url, {
       payload: data
     })
