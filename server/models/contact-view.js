b970be28f38fa93d33f50f4a3ca867004d94d735
Harsh Vasudev <hvasudev@gmail.com>
2021-08-19 23:10:05 +0100

    re reverting the previous summary pages changes revert
    This reverts commit 8c7585ea02a32642d9538ba6814ad06cd9359dbe.

--- server/models/contact-view.js
+++ server/models/contact-view.js
@@ -1,10 +1,10 @@
 function ContactViewModel (data) {
   if (data) {
     this.errorSummary = data.errorSummary
-    this.recipientemail = data.recipientemail
+    this.recipientemail = data.PDFinformationDetailsObject.recipientemail
     this.emailError = data.emailError
     this.fullnameError = data.fullnameError
-    this.fullName = data.fullName
+    this.fullName = data.PDFinformationDetailsObject.fullName
     this.PDFinformationDetailsObject = data.PDFinformationDetailsObject
   } else {
     this.errors = {}
