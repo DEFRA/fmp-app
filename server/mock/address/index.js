65827440d4948e7939906bed955498e9e42c6c7b
Harsh Vasudev <hvasudev@gmail.com>
2022-03-25 14:35:38 +0000

    hardcoded WA41HT

--- server/mock/address/index.js
+++ server/mock/address/index.js
@@ -2,7 +2,7 @@ const addressService = require('../../services/address')
 const findByPlace = require('./find-by-place')
 
 addressService.findByPlace = async (place) => {
-  const result = findByPlace[place]
+  const result = findByPlace['WA41HT']
   if (!result) {
     throw new Error(`findByPlace mock not found for place [${place}]`)
   } else {
