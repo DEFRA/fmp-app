4aca689e69c1283f9809d2526f60cd644e77f326
Harsh Vasudev <hvasudev@gmail.com>
2022-03-25 14:08:07 +0000

    Pre-prod - address mocking

--- server/mock/address/index.js
+++ server/mock/address/index.js
@@ -2,7 +2,7 @@ const addressService = require('../../services/address')
 const findByPlace = require('./find-by-place')
 
 addressService.findByPlace = async (place) => {
-  const result = findByPlace[place.replace('%20', '').toUpperCase()]
+  const result = findByPlace[place]
   if (!result) {
     throw new Error(`findByPlace mock not found for place [${place}]`)
   } else {
