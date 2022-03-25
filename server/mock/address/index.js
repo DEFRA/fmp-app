1cfcaa8f4e31717b0c04fc6a8dd6fbc09843dbad
Harsh Vasudev <hvasudev@gmail.com>
2022-03-25 15:01:11 +0000

    reverting the change

--- server/mock/address/index.js
+++ server/mock/address/index.js
@@ -2,7 +2,7 @@ const addressService = require('../../services/address')
 const findByPlace = require('./find-by-place')
 
 addressService.findByPlace = async (place) => {
-  const result = findByPlace['WA41HT']
+  const result = findByPlace[place.replace('%20', '').toUpperCase()]
   if (!result) {
     throw new Error(`findByPlace mock not found for place [${place}]`)
   } else {
