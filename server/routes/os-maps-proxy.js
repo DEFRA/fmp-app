91490f1096c7e77adcc707166922db2946cc78c1
Harsh Vasudev <hvasudev@gmail.com>
2022-03-29 16:07:12 +0100

    Jmeter performance test os-maps-proxy

--- server/routes/os-maps-proxy.js
+++ server/routes/os-maps-proxy.js
@@ -10,8 +10,13 @@ module.exports = {
   path: '/os-maps-proxy',
   handler: async (request, h) => {
     try {
-      const url = `${osMapsUrl}${request.url.search}&key=${osMapsKey}`
-      const payload = await wreck.get(url)
+      // Mocking Os-maps for Performance Jmeter tests
+      // const url = `${osMapsUrl}${request.url.search}&key=${osMapsKey}`
+      // const payload = await wreck.get(url)
+      const payload = {
+        payload: '/assets/images/marker.png'
+      }
+
       return h.response(payload.payload).type('image/png')
     } catch (err) {
       return Boom.badRequest('os-get-maps failed')
