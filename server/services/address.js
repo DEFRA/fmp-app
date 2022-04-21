4d0592246d8701a3977b285c4918448c18b432d8
Mark Fee <Mark.Fee@defra.gov.uk>
2022-04-21 13:07:27 +0100

    FCRM-2176 findByPlace now returns a locationDetails string along with geometry

--- server/services/address.js
+++ server/services/address.js
@@ -13,9 +13,25 @@ module.exports = {
     }
 
     const gazetteerEntries = payload.results.map(function (item) {
+      const { NAME1, POPULATED_PLACE, DISTRICT_BOROUGH, COUNTY_UNITARY, REGION, COUNTRY } = (item.GAZETTEER_ENTRY || {})
+      const locationArray = [
+        NAME1,
+        POPULATED_PLACE,
+        DISTRICT_BOROUGH,
+        COUNTY_UNITARY,
+        REGION,
+        COUNTRY
+      ].filter((item) => item) // Remove undefined entries
+
+      const locationDetails = locationArray
+        .filter((item, idx) => {
+          return locationArray[idx - 1] !== item
+        }).join(', ') // Remove duplicate entries
+
       return {
         geometry_x: item.GAZETTEER_ENTRY.GEOMETRY_X,
-        geometry_y: item.GAZETTEER_ENTRY.GEOMETRY_Y
+        geometry_y: item.GAZETTEER_ENTRY.GEOMETRY_Y,
+        locationDetails
       }
     })
     return gazetteerEntries
