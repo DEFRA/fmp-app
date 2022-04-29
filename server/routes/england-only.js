749c5d3512573214a0e7939d07db637a9bc2f3ee
Mark Fee <mark.fee@defra.gov.uk>
2022-04-29 10:55:52 +0100

    FCRM-3663 - reinstate original text when not-england is loaded without a search (#238)

--- server/routes/england-only.js
+++ server/routes/england-only.js
@@ -5,9 +5,10 @@ module.exports = {
     description: 'That location is not in England',
     handler: async (request, h) => {
       let { locationDetails = '' } = request.query
-      const { placeOrPostcode = '' } = request.query
+      const { placeOrPostcode = '', easting, northing, nationalGridReference } = request.query
+      const isSearch = !(!placeOrPostcode && !easting && !northing && !nationalGridReference)
       locationDetails = locationDetails.replace(`${placeOrPostcode}, `, '')
-      return h.view('england-only', { locationDetails, placeOrPostcode })
+      return h.view('england-only', { locationDetails, placeOrPostcode, isSearch })
     }
   }
 }
