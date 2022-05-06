f9091d001993bb9cd6d89100f53433487a2842ce
Mark Fee <Mark.Fee@defra.gov.uk>
2022-05-06 12:16:27 +0100

    FCRM-3560 replaced broken privacy-policy test with working privacy-notice test

---
+++ test/routes/privacy-notice.js
@@ -0,0 +1,34 @@
+const Lab = require('@hapi/lab')
+const Code = require('code')
+const lab = exports.lab = Lab.script()
+const createServer = require('../../server')
+const { payloadMatchTest } = require('../utils')
+
+lab.experiment('privacy-notice', () => {
+  let server
+
+  lab.before(async () => {
+    server = await createServer()
+    await server.initialize()
+  })
+
+  lab.after(async () => {
+    await server.stop()
+  })
+
+  const assertPrivacyNoticePage = async (response) => {
+    const { payload } = response
+    Code.expect(response.statusCode).to.equal(200)
+    await payloadMatchTest(payload, /<h1 class="govuk-heading-xl">Privacy notice<\/h1>/g)
+  }
+
+  lab.test('privacy-notice', async () => {
+    const options = {
+      method: 'GET',
+      url: '/privacy-notice'
+    }
+
+    const response = await server.inject(options)
+    await assertPrivacyNoticePage(response)
+  })
+})
