feadff91a304faa01f81febba999ad5155cfda2d
Mark Fee <Mark.Fee@defra.gov.uk>
2022-05-24 14:31:01 +0100

    FCRM-3670 removed server/models/fmp-log-view.js

--- server/models/fmp-log-view.js
+++
@@ -1,8 +0,0 @@
-function FmpLogViewModel (data, errors) {
-  if (data) {
-    this.Error = data.Error
-    this.Message = data.Message
-    this.CorrelationId = data.CorrelationId
-  }
-}
-module.exports = FmpLogViewModel
