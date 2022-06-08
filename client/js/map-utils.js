1bf5de272461a84206273b2ea11cbf34a9064d6d
Mark Fee <mark.fee@defra.gov.uk>
2022-06-08 08:42:45 +0100

    FCRM-3731 added tests to cover mapUtils (#270)
    * FCRM-3731 added tests to cover mapUtils.mapState
    
    * FCRM-3731 removed braces to satisfy code analysis
    
    * FCRM-3731 added a unit test for getTargetUrl
    
    * FCRM-3731 removed braces to satisfy code analysis

--- client/js/map-utils.js
+++ client/js/map-utils.js
@@ -32,6 +32,12 @@ try {
   sessionStorageAvailable = false
 }
 
+const _mockSessionStorageAvailable = value => {
+  const currentValue = sessionStorageAvailable
+  sessionStorageAvailable = value
+  return currentValue
+}
+
 const mapState = {
   getItem: name => sessionStorageAvailable
     ? window.sessionStorage.getItem(name)
@@ -39,8 +45,41 @@ const mapState = {
   setItem: (name, value) => sessionStorageAvailable
     ? window.sessionStorage.setItem(name, value)
     : undefined,
-  removeItem: (name) => sessionStorageAvailable
+  removeItem: name => sessionStorageAvailable
     ? window.sessionStorage.removeItem(name)
     : undefined
 }
-module.exports = { createTileLayer, mapState }
+
+const getCenterOfExtent = extent => {
+  const X = extent[0] + (extent[2] - extent[0]) / 2
+  const Y = extent[1] + (extent[3] - extent[1]) / 2
+  return [parseInt(X, 10), parseInt(Y, 10)]
+}
+
+function getTargetUrl (featureMode, polygon, point, location, fullName, recipientemail) {
+  let coordinates
+  let url = '/flood-zone-results'
+
+  if (featureMode === 'polygon' && polygon) {
+    const geometry = polygon.getGeometry()
+    coordinates = geometry.getCoordinates()[0]
+    const extent = geometry.getExtent()
+    const center = getCenterOfExtent(extent)
+    const coords = JSON.stringify(coordinates.map(function (item) {
+      return [parseInt(item[0], 10), parseInt(item[1], 10)]
+    }))
+    url += '?polygon=' + coords
+    url += '&center=' + JSON.stringify(center)
+  } else {
+    coordinates = point.getGeometry().getCoordinates()
+    url += '?easting=' + parseInt(coordinates[0], 10)
+    url += '&northing=' + parseInt(coordinates[1], 10)
+  }
+  url += '&location=' + location
+  url += '&fullName=' + fullName
+  url += '&recipientemail=' + recipientemail
+
+  return url
+}
+
+module.exports = { createTileLayer, mapState, _mockSessionStorageAvailable, getTargetUrl }
