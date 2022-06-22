0489fc47404102a53305c5e704710831a3af0ca2
Mark Fee <Mark.Fee@defra.gov.uk>
2022-06-22 13:53:17 +0100

    FCRM-3770 added an extra download P1 bullet point to the home page

--- test/routes/home.js
+++ test/routes/home.js
@@ -67,5 +67,7 @@ lab.experiment('home', () => {
 
     await payloadMatchTest(payload, /<a href = "https:\/\/www.ordnancesurvey.co.uk\/">Ordnance Survey\(OS\)<\/a>/g)
     await payloadMatchTest(payload, /<a href="os-terms">Ordnance Survey terms and conditions<\/a>/g)
+    await payloadMatchTest(payload, /<li>download a printable flood map for planning \(PDF\) showing your flood zone \(also known as a product 1\)<\/li>/g)
+    await payloadMatchTest(payload, /<li>request flood risk assessment data \(also known as a product 4\)<\/li>/g)
   })
 })
