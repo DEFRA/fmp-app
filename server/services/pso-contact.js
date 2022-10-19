3efe6c99e8388e41af4f2034e2a2a125438c4234
Mark Fee <Mark.Fee@defra.gov.uk>
2022-10-19 11:28:26 +0100

    FCRM-3916 change the pso-contact.js service to consume the new fmp-service api

--- server/services/pso-contact.js
+++ server/services/pso-contact.js
@@ -1,21 +1,33 @@
+const util = require('../util')
 const config = require('../../config')
-const Wreck = require('@hapi/wreck')
-const url = config.functionAppUrl + '/pso/contacts'
+const url = config.service + '/get-pso-contacts/'
 
-async function getPsoContacts (easting, northing) {
+const getPsoContacts = (easting, northing) => {
   try {
     if (!easting || !northing) {
       throw new Error('No point provided')
     }
-    const data = JSON.stringify({ x: easting, y: northing })
-    const { payload } = await Wreck.post(url, {
-      payload: data
-    })
-    return JSON.parse(payload.toString())
+    // EmailAddress: 'neyorkshire@environment-agency.gov.uk',
+    // AreaName: 'Environment Agency team in Yorkshire',
+    // LocalAuthorities: [ [ 'Ryedale' ] ],
+    // useAutomatedService: true?
+
+    return util.getJson(url + easting + '/' + northing)
+      .then((result) => {
+        console.log('psoContacts result', result)
+        const { emailaddress: EmailAddress, areaname: AreaName, localauthorities: LocalAuthorities, useautomatedservice: useAutomatedService } = result
+        return {
+          EmailAddress,
+          AreaName,
+          LocalAuthorities: [[LocalAuthorities]],
+          useAutomatedService
+        }
+      })
   } catch (error) {
     throw new Error('Fetching Pso contacts failed: ', error)
   }
 }
+
 const expiresIn = 600000 // 10 minutes
 const staleIn = 540000 // 9 minutes
 const generateTimeout = 10000 // 10 seconds
