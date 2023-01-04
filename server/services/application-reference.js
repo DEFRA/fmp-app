954096f01c86c27c176c777a5ad9f27d4f26c3fa
Mark Fee <Mark.Fee@defra.gov.uk>
2023-01-04 15:53:11 +0000

    FCRM-3959 removed redundant service /application-reference.js

--- server/services/application-reference.js
+++
@@ -1,9 +0,0 @@
-const config = require('../../config')
-const url = config.functionAppUrl + '/reference-number'
-const Wreck = require('@hapi/wreck')
-
-const getApplicationReferenceNumber = async function () {
-  const { payload } = await Wreck.get(url)
-  return payload.toString()
-}
-module.exports = { getApplicationReferenceNumber }
