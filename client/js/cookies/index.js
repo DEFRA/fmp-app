06a240f0e435da74e201354cb3e1cbf22ff6ed2f
Mark Fee <Mark.Fee@defra.gov.uk>
2023-06-06 12:48:19 +0100

    FCRM-4257 cookies page reflects GA setting and deletes all GA cookies on reject

--- client/js/cookies/index.js
+++ client/js/cookies/index.js
@@ -1,3 +1,34 @@
+// inferGaCookieDomain is required as GA inconsistently sets the cookies domain
+// for the main PROD site it is .flood-map-for-planning.service.gov.uk
+// but for the dev, tst, pre it is just .defra.cloud
+// and for localhost it is not required
+// without this, cookie deletion wont work
+const inferGaCookieDomain = () => {
+  const domain = document.domain
+  if (domain.match('flood-map-for-planning.service.gov.uk')) {
+    return 'domain=.flood-map-for-planning.service.gov.uk;'
+  }
+  if (domain.match('.defra.cloud')) {
+    return 'domain=.defra.cloud;'
+  }
+  return ''
+}
+
+const deleteGaCookies = analyticsAccount => {
+  const cookies = document.cookie.split(';')
+  cookies.forEach(cookie => {
+    const [name = ''] = cookie.split('=')
+    if (name.match('_gid|_ga')) {
+      document.cookie = `${name}=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;${inferGaCookieDomain()}`
+    }
+  })
+  if (analyticsAccount) {
+    // This is also required, as without it analytics makes one last call,
+    // which reinstates one of the GA cookies, that we just deleted
+    window[`ga-disable-${analyticsAccount}`] = true
+  }
+}
+
 module.exports = window.onload = function () {
   const cookieBanner = document.querySelector('.js-cookies-banner')
   const questionBanner = document.querySelector('.js-question-banner')
@@ -34,6 +65,10 @@ module.exports = window.onload = function () {
     return acc
   }
 
+  window.FMfP = {
+    deleteGaCookies
+  }
+
   function getCookie (key) {
     // Internet Explorer v.<=11 does not support arrow functions, string literals, object destructuring
     const cookies = document.cookie
@@ -53,6 +88,8 @@ module.exports = window.onload = function () {
   const rejectFn = function () {
     setCookie(cookieName, 'Reject', 365)
     questionBanner.setAttribute('hidden', true)
+    // Now we also need to delete any analytics cookies
+    deleteGaCookies()
     showBanner(rejectedBanner)
   }
 
