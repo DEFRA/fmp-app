fba868e1fa7e7099d742289dbe67541209eac85b
Mark Fee <Mark.Fee@defra.gov.uk>
2023-11-23 14:12:26 +0000

    FCRM-4556 difference now FMP_SESSION cookie is set

--- server/routes/home.js
+++ server/routes/home.js
@@ -7,11 +7,19 @@ module.exports = [{
     handler: async (request, h) => {
       if (typeof request.yar === 'undefined' || typeof request.yar.get('displayError') === 'undefined') {
         // return h.view('home', { allowRobots: true, headers: JSON.stringify(request.headers) })
-        const fmpSession = request.state.FMP_SESSION
+        // const fmpSession = request.state.FMP_SESSION || { serverSession: true, random: Math.random() }
+        const fmpSession = request.state.FMP_SESSION || `${Math.random()}`
+        // const { AWSALB, AWSALBAPP, AWSALBTG } = request.state
         const headers = JSON.stringify(request.headers)
         // console.log('\nhome\ Request.headers', request.headers)
         // console.log('\nhome\ Request.headers Stringified', headers)
-        return h.view('home', { allowRobots: true, headers: encodeURIComponent(headers), fmpSession: encodeURIComponent(fmpSession) })
+        const serverCookies = JSON.stringify(request.state)
+        return h.view('home', {
+          allowRobots: true,
+          headers: encodeURIComponent(headers),
+          fmpSession: encodeURIComponent(fmpSession),
+          serverCookies: encodeURIComponent(serverCookies)
+        }).state('FMP_SESSION', fmpSession)
       } else {
         const errMess = request.yar.get('displayError')
         request.yar.set('displayError', {})
