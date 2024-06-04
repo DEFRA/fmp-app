d25f95354bc64dbed6c19eb714d5515d7617979d
Iyanu Akenroye <iyanu.akenroye@defra.gov.uk>
2024-06-04 20:09:03 +0100

    fix for pre

--- server/routes/check-your-details.js
+++ server/routes/check-your-details.js
@@ -171,8 +171,11 @@ module.exports = [
               'Remember if the postcode is part of the previous log, then it sent it to the fmp-api'
             )
             console.log(error.output ? error.output : JSON.stringify(error))
-            const redirectURL = `/order-not-submitted?polygon=${payload.polygon}&center=[${payload.easting},${payload.northing}]&location=${PDFinformationDetailsObject.location}&error=${JSON.stringify(error)}`
-            return h.redirect(redirectURL)
+            // const redirectURL = `/order-not-submitted?polygon=${payload.polygon}&center=[${payload.easting},${payload.northing}]&location=${PDFinformationDetailsObject.location}`
+            return h.view('order-not-submitted', {
+              error: JSON.stringify(error)
+            })
+            // return h.redirect(redirectURL)
           }
 
           // Forward details to confirmation page
