7f725633ae7fb7fd979c44511108bced1866686d
Mark Fee <mark.fee@defra.gov.uk>
2024-10-28 17:13:07 +0000

    FCRM-5311 refactored toBool to avoid nested ternaries (code smell)

--- config/toBool.js
+++ config/toBool.js
@@ -1,6 +1,10 @@
-const toBool = (val) => (val === true || val === 'true')
-  ? true
-  : (val === false || val === 'false')
-      ? false
-      : undefined
+const toBool = (val) => {
+  if (val === true || val === 'true') {
+    return true
+  }
+  if (val === false || val === 'false') {
+    return false
+  }
+  return undefined
+}
 module.exports = { toBool }
