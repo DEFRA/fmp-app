992d829347d02754b18c90d935f31ecd12f8465a
Mark Fee <mark.fee@defra.gov.uk>
2024-11-14 11:14:51 +0000

    FCRM-5349 increased tests to fully cover getProduct1.js

--- server/services/eaMaps/getProduct1.js
+++ server/services/eaMaps/getProduct1.js
@@ -9,6 +9,27 @@ const { getToken } = require('./getToken')
 const https = require('https')
 const httpsAgent = new https.Agent({ keepAlive: true })
 
+const parseEaMapsProduct1Response = (response) => {
+  const { data } = response
+  const { results } = data
+
+  if (!results || !Array.isArray(results)) {
+    const message = 'unexpected results from eaMaps generate pdf'
+    console.log(message, results)
+    throw new Error(message)
+  }
+
+  return results.reduce((response, resultEntry) => {
+    const { paramName, value } = resultEntry
+    if (paramName === 'pdfFile') {
+      response.url = (value && value.url) || undefined
+    } else if (paramName === 'error') {
+      response.error = value
+    }
+    return response
+  }, { url: undefined, error: undefined })
+}
+
 const getProduct1 = async (polygon, referenceNumber = 'X', scale, _holdingComments) => {
   try {
     const token = await getToken()
@@ -25,28 +46,32 @@ const getProduct1 = async (polygon, referenceNumber = 'X', scale, _holdingCommen
       token
     }
     console.log('\n\npdfUrl', pdfUrl)
+    // 1st post the data, which triggers the EAMaps process to produce a temporary pdf
+    // and returns the url of that pdf
     const response = await axios.post(pdfUrl, formData, {
       httpsAgent,
       headers
     })
 
-    const { data } = response
+    const { url, error } = parseEaMapsProduct1Response(response)
 
-    const { results } = data
-
-    if (results) {
-      const { value: { url } } = results[0]
-
-      const response = await axios.get(url, {
-        httpsAgent,
-        responseType: 'stream'
-      })
-
-      const { data: pdfData } = response
-      return pdfData
-    } else {
-      throw new Error(data)
+    if (error) {
+      console.log('An error was returned from the eaMaps Product 1 service', error)
+      throw (error)
+    } else if (!url) {
+      const message = 'The eaMaps Product 1 service failed to return a url'
+      console.log(message)
+      throw new Error(message)
     }
+
+    // Now fetch and return the actual pdf stream using the returned url
+    const pdfStreamResponse = await axios.get(url, {
+      httpsAgent,
+      responseType: 'stream'
+    })
+
+    const { data: pdfData } = pdfStreamResponse
+    return pdfData
   } catch (error) {
     console.log('Error caught in getProduct1', error)
     throw (error)
--- test/routes/product-1.js
+++ test/routes/product-1.js
@@ -4,24 +4,47 @@ const Code = require('@hapi/code')
 const lab = (exports.lab = Lab.script())
 const createServer = require('../../server')
 const axios = require('axios')
+const { config } = require('../../config')
 
 lab.experiment('product-1.js', () => {
   let server
   const AxiosMockAdapter = require('axios-mock-adapter')
   const mockAxios = new AxiosMockAdapter(axios)
 
+  const getProduct1Url = config.eamaps.serviceUrl + config.eamaps.product1EndPoint
+  const getTokenUrl = config.eamaps.serviceUrl + config.eamaps.tokenEndPoint
+  const mockPdfUrl = 'PDF_URL'
+
+  const validPdfResponse = {
+    results: [
+      { paramName: 'pdfFile', dataType: 'GPDataFile', value: { url: mockPdfUrl } },
+      { paramName: 'error', dataType: 'GPString', value: '' }
+    ]
+  }
+
+  const invalidPdfResponse = {
+    results: [
+      { paramName: 'pdfFile', dataType: 'GPDataFile', value: null },
+      { paramName: 'pdfFile', dataType: 'GPDataFile', value: {} }, // increase coverage
+      { paramName: 'error', dataType: 'GPString', value: 'ERR' },
+      { paramName: 'anything', dataType: 'GPString', value: 'for test coverage only' }
+    ]
+  }
+
   lab.before(async () => {
-    mockAxios.onPost('http://dummyEAMapslUrl/tokens/generateToken')
-      .reply(200, { token: 'XXX', expires: 99999999999 })
-
-    mockAxios.onPost('http://dummyEAMapslUrl/rest/services/FMfP/FMFPGetProduct1/GPServer/fmfp_get_product1/execute')
-      .reply(200, { results: [{ value: { url: 'PDF_URL' } }] })
-
-    mockAxios.onGet('PDF_URL')
-      .reply(200, 'PDF_STREAM')
-
     server = await createServer()
     await server.initialize()
+  })
+
+  lab.beforeEach(async () => {
+    mockAxios.onPost(getTokenUrl)
+      .reply(200, { token: 'XXX', expires: 99999999999 })
+
+    mockAxios.onPost(getProduct1Url)
+      .reply(200, validPdfResponse)
+
+    mockAxios.onGet(mockPdfUrl)
+      .reply(200, 'PDF_STREAM')
   })
 
   lab.after(async () => {
@@ -47,7 +70,7 @@ lab.experiment('product-1.js', () => {
   })
 
   lab.test('product-1 route should catch an error when returned by getToken', async () => {
-    mockAxios.onPost('http://dummyEAMapslUrl/tokens/generateToken')
+    mockAxios.onPost(getTokenUrl)
       .reply(200, { token: 'XXX', expires: 99999999999, error: { message: 'Mocked Error' } })
 
     const response = await server.inject({
@@ -56,5 +79,42 @@ lab.experiment('product-1.js', () => {
       payload: product1Payload
     })
     Code.expect(response.statusCode).to.equal(500)
+    // NB The current response when an error occurs is a redirect to a generic error page
+    // FCRM-5373 has been raised to determine a better UX
+  })
+
+  lab.test('product-1 route should catch an error when returned by eamaps getProduct1', async () => {
+    mockAxios.onPost(getProduct1Url).reply(200, invalidPdfResponse)
+
+    const response = await server.inject({
+      method: 'POST',
+      url: '/product-1',
+      payload: product1Payload
+    })
+    Code.expect(response.statusCode).to.equal(500)
+    // NB The current response when an error occurs is a redirect to a generic error page
+    // FCRM-5373 has been raised to determine a better UX
+  })
+
+  // server/services/eaMaps/getProduct1.js missing coverage on line(s): 16-19, 26, 57-63
+  // server/services/eaMaps/getToken.js missing coverage on line(s): 15, 16, 40, 52-54
+  const unexpectedResponses = [
+    [{}, 'no results'],
+    [{ results: 'not an array' }, "results that aren't an array"],
+    [{ results: [] }, 'results that are an empty array']
+  ]
+  unexpectedResponses.forEach(([getProduct1Response, description]) => {
+    lab.test(`product-1 route should catch an error when eamaps getProduct1 returns ${description}`, async () => {
+      mockAxios.onPost(getProduct1Url).reply(200, getProduct1Response)
+
+      const response = await server.inject({
+        method: 'POST',
+        url: '/product-1',
+        payload: product1Payload
+      })
+      Code.expect(response.statusCode).to.equal(500)
+      // NB The current response when an error occurs is a redirect to a generic error page
+      // FCRM-5373 has been raised to determine a better UX
+    })
   })
 })
