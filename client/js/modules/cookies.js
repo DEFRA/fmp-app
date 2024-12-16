9c478b43035fbc17c7344145f3c32f88a8ae0eab
Mark Fee <mark.fee@defra.gov.uk>
2024-12-16 10:14:08 +0000

    FCRM-5408-temp set timeout of dont show again to 30 seconds for testing

--- client/js/modules/cookies.js
+++ client/js/modules/cookies.js
@@ -1,6 +1,7 @@
 export const setCookie = (cName, cValue, expDays) => {
   const date = new Date()
-  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000)
+  // date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000)
+  date.setTime(date.getTime() + expDays * 1000)
   const expires = 'expires=' + date.toUTCString()
   document.cookie = cName + '=' + cValue + '; ' + expires + '; path=/'
 }
