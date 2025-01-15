560b9384a779b04c2c615a7a33614a4d94d91b39
Mark Fee <mark.fee@defra.gov.uk>
2025-01-15 17:08:37 +0000

    FCRM-5448 removed all references to isAreaBenefiting except on fzr.html

--- server/models/flood-zone.js
+++ server/models/flood-zone.js
@@ -1,14 +1,12 @@
 function FloodZoneModel (risk) {
-  this.isAreaBenefiting = false
   this.isZone3 = false
   this.isZone2 = false
   this.isZone1 = false
   // Set the points flood zone (centroid)
-  this.isAreaBenefiting = false
   if (risk.floodzone_3) {
-    this.zone = this.isAreaBenefiting ? 'FZ3a' : 'FZ3'
+    this.zone = 'FZ3'
   } else if (risk.floodzone_2) {
-    this.zone = this.isAreaBenefiting ? 'FZ2a' : 'FZ2'
+    this.zone = 'FZ2'
   } else {
     this.zone = 'FZ1'
   }
