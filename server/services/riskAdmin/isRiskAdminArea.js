0e400f19d020723f1386ad9f851f234683225bcb
Mark Fee <mark.fee@defra.gov.uk>
2025-01-17 17:57:11 +0000

    FCRM-5448 added testCoverage of isRiskAdminArea, enabled local mocking

--- server/services/riskAdmin/isRiskAdminArea.js
+++ server/services/riskAdmin/isRiskAdminArea.js
@@ -4,19 +4,25 @@ const riskAdminApiUrl = config.riskAdminApi.url
 const axios = require('axios')
 
 const isRiskAdminArea = async (polygon) => {
+  // set forceRiskAdminApiResponse to true or false in your local env file
+  // to test the results page without the need to run a local instance of riskadmin-api
+  if (process.env.forceRiskAdminApiResponse !== undefined) {
+    return { isRiskAdminArea: process.env.forceRiskAdminApiResponse === 'true' }
+  }
+
   const { x, y } = getCentreOfPolygon(polygon)
   const url = `${riskAdminApiUrl}/extra_info/${x}/${y}`
 
   try {
     const { data } = await axios.get(url)
-    console.log('isRiskAdminArea - data:\n', data, '\n')
-
-    const response = {
-      isRiskAdminArea: Array.isArray(data) ? data.length > 0 : data
+    if (Array.isArray(data)) {
+      const response = {
+        isRiskAdminArea: data.length > 0
+      }
+      return response
     }
-
-    console.log('isRiskAdminArea - response:\n', response, '\n')
-    return response
+    console.log('riskadmin-api response data:\n', data)
+    throw new Error('Unexpected response from riskadmin-api')
   } catch (error) {
     if (error.message) {
       const { message, name, code } = error
