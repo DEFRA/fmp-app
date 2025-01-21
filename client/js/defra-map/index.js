56f7aed9779617d8eeee7864eba2a0cead9dc079
Mark Fee <mark.fee@defra.gov.uk>
2025-01-21 17:45:19 +0000

    FCRM-5476 Upgrade @defra-map to 0.4.0 (#452)
    * FCRM-5476 attempted an upgrade to 0.4.0 - getting invalid styles error
    
    * FCRM-5476 fixed issue causing map to fail if basemap had an incorrect value
    
    * FCRM-5476 refactor to avoid Sonar issue

--- client/js/defra-map/index.js
+++ client/js/defra-map/index.js
@@ -69,7 +69,20 @@ const surfaceWaterStyleLayers = [
 //   'Risk of Flooding from Surface Water Depth CCSW1 > 900mm/1',
 //   'Risk of Flooding from Surface Water Depth CCSW1 > 1200mm/1'
 // ]
+
+const fixLocalStorage = () => {
+  // Temp fix until 0.5.0
+  // Map will not load if localStorage basemap is not one of default OR dark
+  // but 0.3.0 sets the value to 'default,light', which screws up
+  // the map component after an upgrade to 0.4.0
+  const basemap = window.localStorage.getItem('basemap')
+  if (basemap !== 'default' && basemap !== 'dark') {
+    window.localStorage.removeItem('basemap')
+  }
+}
+
 getDefraMapConfig().then((defraMapConfig) => {
+  fixLocalStorage() // Temp fix until 0.5.0
   const getVectorTileUrl = (layerName) => `${defraMapConfig.agolVectorTileUrl}/${layerName + defraMapConfig.layerNameSuffix}/VectorTileServer`
   const getFeatureLayerUrl = (layerName) => `${defraMapConfig.agolServiceUrl}/${layerName}/FeatureServer`
   const getModelFeatureLayerUrl = (layerName) => `${defraMapConfig.agolServiceUrl}/${layerName + defraMapConfig.layerNameSuffix}/FeatureServer`
@@ -364,7 +377,7 @@ getDefraMapConfig().then((defraMapConfig) => {
     maxZoom: 20,
     centre: [340367, 322766],
     height: '100%',
-    hasGeoLocation: true,
+    hasGeoLocation: false,
     framework: 'esri',
     symbols: [symbols.waterStorageAreas, symbols.floodDefences, symbols.mainRivers],
     requestCallback: getRequest,
