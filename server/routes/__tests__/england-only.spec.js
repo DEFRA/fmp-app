b16693697cd1276b77f6d4d5b4bbb53c280415b7
Tedd <teddmason@gmail.com>
2025-01-21 15:32:07 +0000

    FCRM-5514 - return england-only route to service (#453)

---
+++ server/routes/__tests__/england-only.spec.js
@@ -0,0 +1,52 @@
+const { submitGetRequest } = require('../../__test-helpers__/server')
+const { assertCopy } = require('../../__test-helpers__/copy')
+const constants = require('../../constants')
+
+const url = constants.routes.ENGLAND_ONLY
+
+describe('Triage Page', () => {
+  const getQueries = [{
+    testName: 'Happy get: with postcode',
+    queryParams: {
+      easting: '123456',
+      northing: '123456',
+      locationDetails: 'Some address in Wales',
+      isPostCode: true,
+      placeOrPostcode: 'SY16 1AA'
+    },
+    text: 'Your search for \'SY16 1AA\' has been placed in Some address in Wales'
+  }, {
+    testName: 'Happy get: with place',
+    queryParams: {
+      easting: '123456',
+      northing: '123456',
+      locationDetails: 'Some address in Wales',
+      isPostCode: false,
+      placeOrPostcode: 'Swansea'
+    },
+    text: 'Your search for \'Swansea\' has been placed in Some address in Wales'
+  }, {
+    testName: 'Happy get: with NGR',
+    queryParams: {
+      easting: '123456',
+      northing: '123456',
+      nationalGridReference: 'SN 85981 88534'
+    },
+    text: 'The location you submitted is not in England, or is only partly in England.'
+  }, {
+    testName: 'Happy get: with easting and northing',
+    queryParams: {
+      easting: '123456',
+      northing: '123456'
+    },
+    text: 'The location you submitted is not in England, or is only partly in England.'
+  }]
+  getQueries.forEach(({ testName, queryParams, text }) => {
+    it(testName, async () => {
+      const response = await submitGetRequest({ url: `${url}?${new URLSearchParams(queryParams).toString()}` }, 'This service provides data for locations in England only')
+      document.body.innerHTML = response.payload
+      assertCopy('title', 'This service is for England only - Flood map for planning - GOV.UK')
+      assertCopy('#not-england-page > div > p:nth-child(2)', text)
+    })
+  })
+})
