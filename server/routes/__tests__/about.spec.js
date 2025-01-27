d1f339fa30e53e30c63058dc8236625b5ae2bae3
Mark Fee <mark.fee@defra.gov.uk>
2025-01-27 10:24:21 +0000

    FCRM-5547 added unit tests for about.js

---
+++ server/routes/__tests__/about.spec.js
@@ -0,0 +1,15 @@
+jest.mock('../../services/external-health-check')
+const { submitGetRequest } = require('../../__test-helpers__/server')
+const { assertCopy } = require('../../__test-helpers__/copy')
+const url = '/about'
+
+describe('About Page', () => {
+  it('/about page should contain version numbers as expected', async () => {
+    await submitGetRequest({ url }, 'Flood map for planning')
+    assertCopy('#fmp-app-version', 'Version: v3.0.0-1')
+    assertCopy('#fmp-app-revision', 'Revision: 9256171')
+    assertCopy('#fmp-app-type', 'internal')
+    assertCopy('#fmp-api-version', 'Version: v8.8.8')
+    assertCopy('#fmp-api-revision', 'Revision: 9988776')
+  })
+})
