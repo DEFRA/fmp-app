77e392c07988f4eadeae0d88916c0199164ade9e
Tedd Mason <teddmason@gmail.com>
2025-02-12 07:33:43 +0000

    FCRM-5596 - update likelihoods to odds

--- client/js/defra-map/terms.js
+++ client/js/defra-map/terms.js
@@ -1,11 +1,11 @@
 const terms = {
   likelihood: {
-    rsHigh: 'Rivers and sea 3.3%',
-    rsMedium: 'Rivers 1% Sea 0.5%',
-    rsLow: 'Rivers and sea 0.1%',
-    swHigh: '3.3%',
-    swMedium: '1%',
-    swLow: '0.1%'
+    rsHigh: 'Rivers and sea 1 in 30',
+    rsMedium: 'Rivers 1 in 100, Sea 1 in 200',
+    rsLow: 'Rivers and sea 1 in 1000',
+    swHigh: '1 in 30',
+    swMedium: '1 in 100',
+    swLow: '1 in 1000'
   }
 }
 
