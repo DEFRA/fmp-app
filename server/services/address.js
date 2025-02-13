3ff20d2873256e6e1849b184734de20920305147
Mark Fee <mark.fee@defra.gov.uk>
2025-02-13 14:38:33 +0000

    FCRM-5634 DO_NOT_PR_THIS getPostcodeFromEastingorNorthing returns hard coded postcode

--- server/services/address.js
+++ server/services/address.js
@@ -68,23 +68,25 @@ module.exports = {
     return gazetteerEntries
   },
   getPostcodeFromEastingorNorthing: async (easting, northing) => {
-    try {
-      console.log('About to: getPostcodeFromEastingorNorthing')
-      const uri = `${config.placeApi?.url}?point=${easting},${northing}&key=${config.ordnanceSurvey.osSearchKey}`
-      const payload = await util.getJson(uri)
-      if (!payload?.results?.[0]) {
-        console.error(
-          `=======================================unable to get postcode for easting :${easting} and northing: ${northing} but continuing operation======================`
-        )
-      } else { console.log('postcode retrieved', JSON.stringify(payload?.results[0])) }
-      return payload?.results && payload?.results.length > 0
-        ? payload?.results[0]?.DPA?.POSTCODE
-        : ''
-    } catch (error) {
-      console.error(
-        `unable to get postcode for easting: ${easting} and northing: ${northing}`,
-        error
-      )
-    }
+    // DO_NOT_MERGE_TO_RELEASE
+    return 'NG1 1AA'
+    // try {
+    //   console.log('About to: getPostcodeFromEastingorNorthing')
+    //   const uri = `${config.placeApi?.url}?point=${easting},${northing}&key=${config.ordnanceSurvey.osSearchKey}`
+    //   const payload = await util.getJson(uri)
+    //   if (!payload?.results?.[0]) {
+    //     console.error(
+    //       `=======================================unable to get postcode for easting :${easting} and northing: ${northing} but continuing operation======================`
+    //     )
+    //   } else { console.log('postcode retrieved', JSON.stringify(payload?.results[0])) }
+    //   return payload?.results && payload?.results.length > 0
+    //     ? payload?.results[0]?.DPA?.POSTCODE
+    //     : ''
+    // } catch (error) {
+    //   console.error(
+    //     `unable to get postcode for easting: ${easting} and northing: ${northing}`,
+    //     error
+    //   )
+    // }
   }
 }
