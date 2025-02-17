f710c3115fc915ee19c2fd67ce4b967d90f07549
Mark Fee <mark.fee@defra.gov.uk>
2025-02-17 13:41:40 +0000

    FCRM-5602 copied next steps page from protototype

---
+++ server/routes/next-steps.js
@@ -0,0 +1,28 @@
+const { config } = require('../../config')
+// const { getAreaInHectares, getCentreOfPolygon } = require('../services/shape-utils')
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/next-steps',
+    options: {
+      description: 'Results Page',
+      handler: async (request, h) => {
+        const { polygon } = request.query
+        const contactData = await request.server.methods.getPsoContactsByPolygon(polygon)
+        const showOrderProduct4Button = config.appType === 'internal' || contactData.useAutomatedService === true
+        const floodData = await request.server.methods.getFloodZoneByPolygon(polygon)
+        // floodData.areaInHectares = getAreaInHectares(polygon)
+        // floodData.centreOfPolygon = getCentreOfPolygon(polygon)
+        // floodData.isFZ1Andlt1ha = floodData.floodZone === '1' && floodData.areaInHectares < 1
+        // floodData.isFZ1Andgt1ha = floodData.floodZone === '1' && floodData.areaInHectares >= 1
+        // floodData.hasROFRSRiskBand =
+        //   floodData.riversAndSeaDefended.riskBandPercent ||
+        //   floodData.riversAndSeaUndefended.riskBandPercent ||
+        //   floodData.riversAndSeaDefendedCC.riskBandPercent ||
+        //   floodData.riversAndSeaUndefendedCC.riskBandPercent
+        return h.view('next-steps', { polygon, floodData, contactData, showOrderProduct4Button })
+      }
+    }
+  }
+]
