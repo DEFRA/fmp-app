5065bf7971f85f13f68d7ec5957a12918baae7d2
Mark Fee <mark.fee@defra.gov.uk>
2025-02-28 11:25:28 +0000

    FCRM-5678 updated map likelihoodchance to match prototype

--- client/js/defra-map/terms.js
+++ client/js/defra-map/terms.js
@@ -16,12 +16,12 @@ const terms = {
     swLow: '1 in 1000'
   },
   likelihoodchance: {
-    rsHigh: '<p class="govuk-body-s">Rivers and sea 3.3%</br>(Rivers and sea 1 in 30)</p>',
-    rsMedium: 'Rivers 1% Sea 0.5%</br>(Rivers 1 in 100, Sea 1 in 200)',
-    rsLow: '<p class="govuk-body-s">Rivers and sea 0.1%</br>(1 in 1000)</p>',
-    swHigh: '3.3% (1 in 30)',
-    swMedium: '1% (1 in 100)',
-    swLow: '0.1% (1 in 1000)'
+    rsHigh: '<p class="govuk-body-s">3.3% (1 in 30)</br>chance of flooding each year </br><details class="govuk-details govuk-!-font-size-16""> <summary class="govuk-details__summary"> <span class="govuk-details__summary-text">About this data</span></summary><div class="govuk-details__text">This data is shown to help local authorities define the functional flood plain, flood zone 3b</div></details></p>',
+    rsMedium: '<p class="govuk-body-s">Rivers 1% (1 in 100)</br>Sea 0.5% (1 in 200)</br>chance of flooding each year</p>',
+    rsLow: '<p class="govuk-body-s">0.1% (1 in 1000)</br>chance of flooding each year</p>',
+    swHigh: '3.3% (1 in 30)</br>chance of flooding each year',
+    swMedium: '1% (1 in 100)</br>chance of flooding each year',
+    swLow: '0.1% (1 in 1000)</br>chance of flooding each year'
   }
 }
 
