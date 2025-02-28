220a97789c684bfdad4abdad0712529dc61c93d8
Mark Fee <mark.fee@defra.gov.uk>
2025-02-28 11:59:03 +0000

    FCRM-5678 removed un-used likelihood

--- client/js/defra-map/index.js
+++ client/js/defra-map/index.js
@@ -121,120 +121,90 @@ getDefraMapConfig().then((defraMapConfig) => {
       name: 'Rivers_1_in_30_Sea_1_in_30_Defended',
       q: '',
       styleLayers: ['Rivers 1 in 30 Sea 1 in 30 Defended/1'],
-      // likelihoodLabel: terms.likelihood.rsHigh,
-      // chanceLabel: terms.chance.rsHigh,
       likelihoodchanceLabel: terms.likelihoodchance.rsHigh
     },
     {
       name: 'Rivers_1_in_30_Sea_1_in_30_Defended_Depth',
       q: 'rsdpdhr',
       styleLayers: ['Rivers 1 in 30 Sea 1 in 30 Defended Depth/1'],
-      // likelihoodLabel: terms.likelihood.rsHigh,
-      // chanceLabel: terms.chance.rsHigh
       likelihoodchanceLabel: terms.likelihoodchance.rsHigh
     },
     {
       name: 'Rivers_1_in_100_Sea_1_in_200_Defended_Depth',
       q: 'rsdpdmr',
       styleLayers: ['Rivers 1 in 100 Sea 1 in 200 Defended Depth/1'],
-      // likelihoodLabel: terms.likelihood.rsMedium,
-      // chanceLabel: terms.chance.rsMedium
       likelihoodchanceLabel: terms.likelihoodchance.rsMedium
     },
     {
       name: 'Rivers_1_in_100_Sea_1_in_200_Undefended_Depth',
       q: 'rsupdmr',
       styleLayers: ['Rivers 1 in 100 Sea 1 in 200 Undefended Depth/1'],
-      // likelihoodLabel: terms.likelihood.rsMedium,
-      // chanceLabel: terms.chance.rsMedium
       likelihoodchanceLabel: terms.likelihoodchance.rsMedium
     },
     {
       name: 'Rivers_1_in_1000_Sea_1_in_1000_Defended_Depth',
       q: 'rsdpdlr',
       styleLayers: ['Rivers 1 in 1000 Sea 1 in 1000 Defended Depth/1'],
-      // likelihoodLabel: terms.likelihood.rsLow,
-      // chanceLabel: terms.chance.rsLow
       likelihoodchanceLabel: terms.likelihoodchance.rsLow
     },
     {
       name: 'Rivers_1_in_1000_Sea_1_in_1000_Undefended_Depth',
       q: 'rsupdlr',
       styleLayers: ['Rivers 1 in 1000 Sea 1 in 1000 Undefended Depth/1'],
-      // likelihoodLabel: terms.likelihood.rsLow,
-      // chanceLabel: terms.chance.rsLow
       likelihoodchanceLabel: terms.likelihoodchance.rsLow
     },
     {
       name: 'Rivers_1_in_30_Sea_1_in_30_Defended_CCP1',
       q: '',
       styleLayers: ['Rivers 1 in 30 Sea 1 in 30 Defended CCP1/1'],
-      // likelihoodLabel: terms.likelihood.rsHigh,
-      // chanceLabel: terms.chance.rsHigh
       likelihoodchanceLabel: terms.likelihoodchance.rsHigh
     },
     {
       name: 'Rivers_1_in_30_Sea_1_in_30_Defended_Depth_CCP1',
       q: 'rsdclhr',
       styleLayers: ['Rivers 1 in 30 Sea 1 in 30 Defended Depth CCP1/1'],
-      // likelihoodLabel: terms.likelihood.rsHigh,
-      // chanceLabel: terms.chance.rsHigh
       likelihoodchanceLabel: terms.likelihoodchance.rsHigh
     },
     {
       name: 'Rivers_1_in_100_Sea_1_in_200_Defended_Depth_CCP1',
       q: 'rsdclmr',
       styleLayers: ['Rivers 1 in 100 Sea 1 in 200 Defended Depth CCP1/1'],
-      // likelihoodLabel: terms.likelihood.rsMedium,
-      // chanceLabel: terms.chance.rsMedium
       likelihoodchanceLabel: terms.likelihoodchance.rsMedium
     },
     {
       name: 'Rivers_1_in_100_Sea_1_in_200_Undefended_Depth_CCP1',
       q: 'rsuclmr',
       styleLayers: ['Rivers 1 in 100 Sea 1 in 200 Undefended Depth CCP1/1'],
-      // likelihoodLabel: terms.likelihood.rsMedium,
-      // chanceLabel: terms.chance.rsMedium
       likelihoodchanceLabel: terms.likelihoodchance.rsMedium
     },
     {
       name: 'Rivers_1_in_1000_Sea_1_in_1000_Defended_Depth_CCP1',
       q: 'rsdcllr',
       styleLayers: ['Rivers 1 in 1000 Sea 1 in 1000 Defended Depth CCP1/1'],
-      // likelihoodLabel: terms.likelihood.rsLow,
-      // chanceLabel: terms.chance.rsLow
       likelihoodchanceLabel: terms.likelihoodchance.rsLow
     },
     {
       name: 'Rivers_1_in_1000_Sea_1_in_1000_Undefended_Depth_CCP1',
       q: 'rsucllr',
       styleLayers: ['Rivers 1 in 1000 Sea 1 in 1000 Undefended Depth CCP1/1'],
-      // likelihoodLabel: terms.likelihood.rsLow,
-      // chanceLabel: terms.chance.rsLow
       likelihoodchanceLabel: terms.likelihoodchance.rsLow
     },
     {
       name: 'Risk_of_Flooding_from_Surface_Water_Low',
       q: 'swlr',
       styleLayers: surfaceWaterStyleLayers,
-      // likelihoodLabel: terms.likelihood.swLow,
-      // chanceLabel: terms.chance.swLow
       likelihoodchanceLabel: terms.likelihoodchance.swLow
     },
     {
       name: 'Risk_of_Flooding_from_Surface_Water_Medium',
       q: 'swmr',
       styleLayers: surfaceWaterStyleLayers,
-      // likelihoodLabel: terms.likelihood.swMedium,
-      // chanceLabel: terms.chance.swMedium
       likelihoodchanceLabel: terms.likelihoodchance.swMedium
     },
     {
       name: 'Risk_of_Flooding_from_Surface_Water_High',
       q: 'swhr',
       styleLayers: surfaceWaterStyleLayers,
-      // likelihoodLabel: terms.likelihood.swHigh,
-      // chanceLabel: terms.chance.swHigh
       likelihoodchanceLabel: terms.likelihoodchance.swHigh
     }
     // ,
--- client/js/defra-map/terms.js
+++ client/js/defra-map/terms.js
@@ -1,12 +1,4 @@
 const terms = {
-  likelihood: {
-    rsHigh: 'Rivers and sea 3.3%',
-    rsMedium: 'Rivers 1% Sea 0.5%',
-    rsLow: 'Rivers and sea 0.1%',
-    swHigh: '3.3%',
-    swMedium: '1%',
-    swLow: '0.1%'
-  },
   chance: {
     rsHigh: 'Rivers and sea 1 in 30',
     rsMedium: 'Rivers 1 in 100, Sea 1 in 200',
