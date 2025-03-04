4dfeca00e13206aff5f095829c1c773f3b70c223
Tedd Mason <teddmason@gmail.com>
2025-03-04 13:47:44 +0000

    updating test name

--- server/routes/__tests__/flood-zone-results-explained.spec.js
+++ server/routes/__tests__/flood-zone-results-explained.spec.js
@@ -2,8 +2,8 @@ const { submitGetRequest } = require('../../__test-helpers__/server')
 const constants = require('../../constants')
 const url = constants.routes.FLOOD_ZONE_RESULTS_EXPLAINED
 
-describe('About Page', () => {
-  it(url, async () => {
+describe(url, () => {
+  it('GET', async () => {
     await submitGetRequest({ url }, 'Flood zones and what they mean')
   })
 })
