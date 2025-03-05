27ee03e2a3512fc4ad606b4098d98c9939bc37e2
Mark Fee <mark.fee@defra.gov.uk>
2025-03-05 16:46:05 +0000

    FCRM-5688 updated markup for map's site-boundary help (#519)

---
+++ client/js/defra-map/markUpItems.js
@@ -0,0 +1,19 @@
+const siteBoundaryHelp = `
+  <h3 class="govuk-heading-m govuk-!-font-size-16">
+    For an approximate site boundary
+  </h3> 
+  <ul class="govuk-list govuk-list--bullet govuk-!-font-size-16">
+    <li>use the red square to define the boundary of your site</li>
+    <li>zoom and move the map to position the square</li>
+    <li>click the ‘add boundary’ button to finish</li>
+  </ul>
+  <h3 class="govuk-heading-m govuk-!-font-size-16">
+    For a more detailed site boundary:
+  </h3>
+  <ul class="govuk-list govuk-list--bullet govuk-!-font-size-16">
+    <li>click ‘edit shape’ and dots will appear on the square</li>
+    <li>move the dots to change the shape of the square until it matches your boundary</li>
+    <li>click the ‘add boundary’ button to finish</li>
+  </ul>`
+
+export { siteBoundaryHelp }
