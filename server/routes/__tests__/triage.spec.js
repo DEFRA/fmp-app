3f0110c80ae700ef2d2c0e4235a80f82c4ef4478
Mark Fee <mark.fee@defra.gov.uk>
2025-03-10 12:29:30 +0000

    FCRM-5727 updated link text on triage page

--- server/routes/__tests__/triage.spec.js
+++ server/routes/__tests__/triage.spec.js
@@ -3,6 +3,7 @@ const {
   submitPostRequest,
   submitPostRequestExpectHandledError
 } = require('../../__test-helpers__/server')
+const { assertCopy } = require('../../__test-helpers__/copy')
 
 const constants = require('../../constants')
 
@@ -18,26 +19,37 @@ describe('Triage Page', () => {
   const radioItems = [
     {
       id: 'location',
-      expectedLocation: '/location'
+      expectedLocation: '/location',
+      expectedLabel: 'For planning purposes or scoping a site',
+      expectedHint: 'You will be taken to the flood map for planning service'
     },
     {
       id: 'buy-sell',
-      expectedLocation: 'https://www.gov.uk/check-long-term-flood-risk'
+      expectedLocation: 'https://www.gov.uk/check-long-term-flood-risk',
+      expectedLabel: 'For buying, selling or valuing a property',
+      expectedHint: 'You will be taken to the check your long term flood risk service'
     },
     {
       id: 'flood-history',
-      expectedLocation: 'https://www.gov.uk/request-flooding-history'
+      expectedLocation: 'https://www.gov.uk/request-flooding-history',
+      expectedLabel: 'To find out if your property is in an area that has flooded',
+      expectedHint: 'You will be taken to information on how to request a flood history report'
+
     },
     {
       id: 'insurance',
-      expectedLocation: 'https://www.gov.uk/check-long-term-flood-risk'
+      expectedLocation: 'https://www.gov.uk/check-long-term-flood-risk',
+      expectedLabel: 'For insurance purposes, to find out if I am at risk of flooding',
+      expectedHint: 'You will be taken to the check your long term flood risk service'
     },
     {
       id: 'other',
-      expectedLocation: 'https://www.gov.uk/browse/environment-countryside/flooding-extreme-weather'
+      expectedLocation: 'https://www.gov.uk/browse/environment-countryside/flooding-extreme-weather',
+      expectedLabel: 'My reason is not listed here',
+      expectedHint: 'Find other flood information on GOV.UK'
     }
   ]
-  radioItems.forEach(({ id, expectedLocation }) => {
+  radioItems.forEach(({ id, expectedLocation, expectedLabel, expectedHint }) => {
     it(`should update continue button to ${expectedLocation} when ${id} is clicked`, async () => {
       const options = {
         url,
@@ -47,6 +59,13 @@ describe('Triage Page', () => {
       }
       const response = await submitPostRequest(options)
       expect(response.headers.location).toBe(expectedLocation)
+    })
+
+    it(`Item ${id} should contain label ${expectedLabel} and hint: ${expectedHint}`, async () => {
+      const response = await submitGetRequest({ url }, 'What flood information do you need?')
+      document.body.innerHTML = response.payload
+      assertCopy(`[for=${id}]`, expectedLabel)
+      assertCopy(`#${id}-item-hint`, expectedHint)
     })
   })
 
