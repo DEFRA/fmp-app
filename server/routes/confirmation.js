b39aa594522aa8509b01ade4222bbf9d421cd92d
Tedd <teddmason@gmail.com>
2025-03-20 07:09:58 +0000

    FCRM-5665 - confirmation refactor and test coverage (#549)

--- server/routes/confirmation.js
+++ server/routes/confirmation.js
@@ -1,4 +1,4 @@
-const Boom = require('@hapi/boom')
+const Joi = require('joi')
 const { punctuateAreaName } = require('../services/punctuateAreaName')
 
 module.exports = {
@@ -7,33 +7,37 @@ module.exports = {
   options: {
     description: 'Get confirmation page for product 4',
     handler: async (request, h) => {
-      try {
-        const {
-          polygon,
-          recipientemail,
-          applicationReferenceNumber,
-          zoneNumber
-        } = request.query
+      const {
+        polygon,
+        recipientemail,
+        applicationReferenceNumber,
+        zoneNumber
+      } = request.query
 
-        const {
-          EmailAddress: psoEmailAddress,
-          AreaName: areaName,
-          LocalAuthorities: localAuthority
-        } = await request.server.methods.getPsoContactsByPolygon(polygon)
+      const {
+        EmailAddress: psoEmailAddress,
+        AreaName: areaName,
+        LocalAuthorities: localAuthority
+      } = await request.server.methods.getPsoContactsByPolygon(polygon)
 
-        const model = {
-          recipientemail,
-          applicationReferenceNumber: applicationReferenceNumber.replace(/(\w{4})/g, '$1 ').replace(/(^\s+|\s+$)/, ''),
-          psoEmailAddress,
-          AreaName: punctuateAreaName(areaName),
-          LocalAuthorities: localAuthority,
-          zoneNumber,
-          polygon
-        }
-        return h.view('confirmation', model)
-      } catch (err) {
-        return Boom.badImplementation(err.message, err)
+      const model = {
+        recipientemail,
+        applicationReferenceNumber: applicationReferenceNumber.replace(/(\w{4})/g, '$1 ').replace(/(^\s+|\s+$)/, ''),
+        psoEmailAddress,
+        areaName: punctuateAreaName(areaName),
+        localAuthority,
+        zoneNumber,
+        polygon
       }
+      return h.view('confirmation', model)
+    },
+    validate: {
+      query: Joi.object({
+        polygon: Joi.string().required(),
+        recipientemail: Joi.string().email().required(),
+        applicationReferenceNumber: Joi.string().required(),
+        zoneNumber: Joi.string().required()
+      })
     }
   }
 }
