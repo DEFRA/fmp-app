692a1e8855cc2b5fdcfaab790ed5a98d5fe6a2d0
Tedd Mason <teddmason@gmail.com>
2025-03-20 09:58:09 +0000

    FCRM-5666 refactor and test contacts route

--- test/models/contact-view.js
+++
@@ -1,9 +0,0 @@
-const Lab = require('@hapi/lab')
-const Code = require('@hapi/code')
-const lab = (exports.lab = Lab.script())
-const contactviewmodel = require('./../../server/models/contact-view')
-lab.experiment('contact view model test', () => {
-  lab.test('contact view model function ', () => {
-    Code.expect(contactviewmodel).to.be.a.function()
-  })
-})
