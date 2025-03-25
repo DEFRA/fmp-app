6a0502596f2c9f13bab6a511658e4f77b40e8cab
Tedd Mason <teddmason@gmail.com>
2025-03-25 08:09:51 +0000

    fixing feedback test for github actions run

--- server/routes/__tests__/feedback.spec.js
+++ server/routes/__tests__/feedback.spec.js
@@ -8,12 +8,19 @@ const constants = require('../../constants')
 const url = constants.routes.FEEDBACK
 
 describe('Feedback', () => {
-  it('Should return feedback page without ref being set', async () => {
+  it('Should return feedback page with ref being set as feedback', async () => {
+    const server = getServer()
+    server.ext('onPreHandler', (request, h) => {
+      request.info.referrer = 'http://localhost:3000/feedback'
+      request.server.info.protocol = 'https'
+      request.info.host = 'localhost:3000'
+      return h.continue
+    })
     const response = await submitGetRequest({ url })
     expect(response.result).toMatchSnapshot()
   })
 
-  it('Should return feedback page with ref being set', async () => {
+  it('Should return feedback page with ref being set without feedback', async () => {
     const server = getServer()
     server.ext('onPreHandler', (request, h) => {
       request.info.referrer = 'http://localhost:3000/map'
@@ -25,11 +32,25 @@ describe('Feedback', () => {
   })
 
   it('Should return feedback page with userAgent being set', async () => {
+    const server = getServer()
+    server.ext('onPreHandler', (request, h) => {
+      request.info.referrer = ''
+      request.server.info.protocol = 'https'
+      request.info.host = 'localhost:3000'
+      return h.continue
+    })
     const response = await submitGetRequest({ url, headers: { 'user-agent': 'test agent' } })
     expect(response.result).toMatchSnapshot()
   })
 
   it('Should return feedback page with no userAgent', async () => {
+    const server = getServer()
+    server.ext('onPreHandler', (request, h) => {
+      request.info.referrer = ''
+      request.server.info.protocol = 'https'
+      request.info.host = 'localhost:3000'
+      return h.continue
+    })
     const response = await submitGetRequest({ url, headers: { 'user-agent': undefined } })
     expect(response.result).toMatchSnapshot()
   })
