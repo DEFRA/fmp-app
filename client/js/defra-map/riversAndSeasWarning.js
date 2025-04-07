966f1ff53e5d05aca8d45569239a6890585bd8d8
Mark Fee <mark.fee@defra.gov.uk>
2025-04-07 16:15:39 +0100

    FCRM-5840 restyled with dans help

--- client/js/defra-map/riversAndSeasWarning.js
+++ client/js/defra-map/riversAndSeasWarning.js
@@ -1,40 +1,43 @@
 const bannerMarkUp = `
 <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
-<strong class="govuk-warning-text__text">
-  <span class="govuk-visually-hidden">Warning</span> Rivers and sea data may show inconsistent results.
-</strong>
-<a id="rivers-and-seas-link" class="fm-c-btn" href="/how-to-use-rivers-and-sea-data">Find out more</a>
+<div class='govuk-warning-text__inner'>
+  <strong class="govuk-warning-text__text">
+    <span class="govuk-visually-hidden">Warning</span> Rivers and sea data may show inconsistent results.
+  </strong>
+  <span class="govuk-warning-text__link">
+    <a id="rivers-and-seas-link" class="govuk-link" href="/how-to-use-rivers-and-sea-data">Find out more</a>
+  </span>
+</div>
 `
 
 const infoPopUpMarkUp = `
-<div class="fm-c-panel__body" tabindex="-1">
-  <div class="fm-c-panel__content" id='rivers-and-seas-disclaimer-panel'>
-    <p class="govuk-body">
-      Rivers and sea supporting data is given to help you further investigate flood risk.
-    </p>
-    <p class="govuk-body">
-      <a href="/how-to-use-rivers-and-sea-data">Find out more about this data and how it should be used.</a>
-    </p>
-    <p class="govuk-body"> In some locations the rivers and sea supporting data may show inconsistent results. </p>
-    <p class="govuk-body"> The flood zones are not affected by this issue. </p>
-  </div>
-</div>`
+  <p class="govuk-body" id='rivers-and-seas-disclaimer-panel'>
+    Rivers and sea supporting data is given to help you further investigate flood risk.
+  </p>
+  <p class="govuk-body">
+    <a href="/how-to-use-rivers-and-sea-data">Find out more about this data and how it should be used.</a>
+  </p>
+  <p class="govuk-body"> In some locations the rivers and sea supporting data may show inconsistent results. </p>
+  <p class="govuk-body"> The flood zones are not affected by this issue. </p>`
 
 const state = { infoDismissed: false }
 
 const bannerElement = document.createElement('div')
 bannerElement.setAttribute('class', 'govuk-warning-text')
 bannerElement.setAttribute('id', 'rivers-and-seas-banner')
-bannerElement.innerHTML = bannerMarkUp
+bannerElement.setAttribute('role', 'status')
+
 
 const showBanner = (show) => {
   const mapElement = document.querySelector('#map')
   if (show) {
+    bannerElement.innerHTML = bannerMarkUp
     mapElement.classList.add('rivers-and-seas-disclaimer')
   } else {
+    bannerElement.innerHTML = ''
     mapElement.classList.remove('rivers-and-seas-disclaimer')
   }
-  bannerElement.style.display = show ? 'flex' : 'none'
+  // bannerElement.style.display = show ? 'flex' : 'none'
 }
 
 showBanner(false)
@@ -61,9 +64,9 @@ const isRiversAndSeas = () => state?.mapState?.segments.find((item) => item === 
 const initialiseRiversAndSeasWarnings = (mapState, floodMap) => {
   Object.assign(state, { mapState, floodMap })
 
-  const mapElement = document.querySelector('#map .fm-o-container .fm-o-side')
+  const mapElement = document.querySelector('#map .fm-o-container .fm-o-main')
   if (mapElement) {
-    mapElement.insertAdjacentElement('afterend', bannerElement)
+    mapElement.insertAdjacentElement('beforebegin', bannerElement)
   }
   onRiversAndSeasMenuItem()
 }
