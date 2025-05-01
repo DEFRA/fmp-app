5ab614b57189e956d77603ade1f76f20a0acc20a
Tedd Mason <teddmason@gmail.com>
2025-05-01 14:45:10 +0100

    Adding capability of uploading WGS84 and web mercator

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -1,9 +1,16 @@
 const multiparty = require('multiparty')
 const gdal = require('gdal-async')
+const fsSync = require('fs')
 const fs = require('fs/promises')
 const path = require('path')
 const os = require('os')
+const proj4 = require('proj4')
 const constants = require('../constants')
+
+// Initialise EPSG:27700 projection for proj4
+const OSTN15Buffer = fsSync.readFileSync('OSTN15_NTv2_OSGBtoETRS.gsb').buffer
+proj4.nadgrid('OSTN15_NTv2_OSGBtoETRS', OSTN15Buffer)
+proj4.defs('EPSG:27700', '+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +units=m +no_defs +nadgrids=OSTN15_NTv2_OSGBtoETRS')
 
 const handlers = {
   get: async (_request, h) => h.view(constants.views.UPLOAD),
@@ -28,12 +35,6 @@ const handlers = {
       vsizipPath = `/vsizip/${zipPath}`
     }
 
-    // validation
-    // is srs 27700
-    // has 1 layer
-    // has 1 feature
-    // size of feature?
-
     // Either open the temp stored zip file or the buffer
     const ds = await gdal.openAsync(vsizipPath || buffer)
 
@@ -44,8 +45,10 @@ const handlers = {
       })
     }
 
+    const srs = ds.layers.get(0).srs.getAuthorityCode()
+
     const polygon = JSON.parse(ds.layers.get(0).features.first().getGeometry().toJSON()).coordinates[0].map(coordinate => {
-      return [coordinate[0], coordinate[1]]
+      return transformPointToOSGB([coordinate[0], coordinate[1]], srs)
     })
 
     // Clean up temp file
@@ -83,6 +86,13 @@ const streamToBuffer = async (stream) => {
   return Buffer.concat(chunks)
 }
 
+const transformPointToOSGB = (point, srs) => {
+  if (srs === '4326' || srs === '3857') {
+    return proj4(`EPSG:${srs}`, 'EPSG:27700', point)
+  }
+  return point
+}
+
 const validateFile = (file) => {
   const errorSummary = []
   const fileExt = file.filename.split('.').pop()
@@ -112,9 +122,9 @@ const validateDataset = (ds) => {
     })
   }
 
-  if (ds.layers.get(0).srs.getAuthorityCode() !== '27700') {
+  if (ds.layers.get(0).srs.getAuthorityCode() !== '27700' && ds.layers.get(0).srs.getAuthorityCode() !== '4326' && ds.layers.get(0).srs.getAuthorityCode() !== '3857') {
     errorSummary.push({
-      text: 'Coordinate system must be OSGB36 EPSG:27700',
+      text: 'Coordinate system must be OSGB36, WGS84 or Web Mercator',
       href: '#boundary'
     })
   }
