3a7895cf5200eec26be99bd1b09cdcee34465c6f
Mark Fee <mark.fee@defra.gov.uk>
2025-05-13 15:06:46 +0100

    FCRM-5948 added tests to cover getEAMapsToken

---
+++ server/services/__tests__/getEAMapsToken.spec.js
@@ -0,0 +1,43 @@
+const { getEAMapsToken, invalidateToken } = require('../eaMaps/getEAMapsToken')
+const axios = require('axios')
+jest.mock('axios')
+const expectedToken = 'MY_TEST_TOKEN'
+
+// uncovered was
+// 13-14,19-24,28-53
+//             44-46, 52-53
+describe('getEAMapsToken', () => {
+  it('should call axios.post', async () => {
+    axios.post.mockResolvedValue({
+      data: {
+        token: expectedToken,
+        expires: 1747147870038,
+        error: undefined
+      }
+    })
+
+    const token = await getEAMapsToken()
+    expect(token).toEqual(expectedToken)
+    const tokenAgain = await getEAMapsToken()
+    expect(tokenAgain).toEqual(expectedToken)
+    expect(axios.post).toHaveBeenCalledTimes(1)
+  })
+
+  it('should handle an axios.post error', async () => {
+    invalidateToken()
+    axios.post.mockResolvedValue({
+      data: {
+        token: expectedToken,
+        expires: 1747147870038,
+        error: new Error('TEST_ERROR')
+      }
+    })
+    try {
+      const token = await getEAMapsToken()
+      expect(token).toEqual('LINE_NOT_REACHED')
+    } catch (error) {
+      expect(error).toEqual(new Error('An error was returned attempting to get an EA Maps esri token'))
+      expect(axios.post).toHaveBeenCalledTimes(1)
+    }
+  })
+})
