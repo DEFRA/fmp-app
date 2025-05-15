ba49654e273c2243584bc26a96127c0a0152efdf
Mark Fee <mark.fee@defra.gov.uk>
2025-05-15 10:44:33 +0100

    FCRM-5956 added Environment to about page (#589)
    * FCRM-5956 added Environment to about page
    
    * FCRM-5956 improved coverage
    
    * FCRM-5956 sonar is really getting on my nerves now

--- server/services/external-health-check.js
+++ server/services/external-health-check.js
@@ -3,20 +3,32 @@ const { config } = require('../../config')
 
 const GIT_REVISION_LENGTH = 7
 
+const environmentName = (env) => ({
+  dev: 'Development',
+  test: 'Test',
+  pre: 'Pre-Production',
+  'prod-green': 'Production Green',
+  'prod-blue': 'Production Blue'
+}[env])
+
+const NOT_AVAILABLE = 'not available'
+
 const getExternalVersion = async (url, name) => {
   try {
     const { data = {} } = await axios.get(url)
-    const { version = '', revision = '' } = data
+    const { version = '', revision = '', environment = '' } = data
     return {
       version,
-      revision: revision.substring(0, GIT_REVISION_LENGTH)
+      revision: revision.substring(0, GIT_REVISION_LENGTH),
+      environment: environmentName(environment)
     }
   } catch (error) {
     console.log(`error fetching ${name} health-check`, url, error)
   }
   return {
-    version: 'not available',
-    revision: 'not available'
+    version: NOT_AVAILABLE,
+    revision: NOT_AVAILABLE,
+    environment: NOT_AVAILABLE
   }
 }
 
