9a784eecec85dc4f4df4e8582dfaba89700f2a87
Michael Steinacher <steinacherm@hotmail.co.uk>
2025-11-05 14:00:13 +0000

    Made changes where p1 download buttons are to disable when times given from API. Tests need writing.

--- server/routes/next-steps.js
+++ server/routes/next-steps.js
@@ -1,4 +1,6 @@
 const { config } = require('../../config')
+const pauseP1URL = config.functionAppUrl + '/pause-p1-download'
+const wreck = require('@hapi/wreck')
 const { isRiskAdminArea } = require('../services/riskAdmin/isRiskAdminArea')
 const {
   getCentreOfPolygon,
@@ -12,17 +14,29 @@ module.exports = [
     options: {
       description: 'Results Page',
       handler: async (request, h) => {
+        let pauseP1DownloadFrom, pauseP1DownloadTo
         const { polygon, encodedPolygon } = checkParamsForPolygon(request.query)
         const [contactData, floodData, { isRiskAdminArea: isRiskAdmin }] = await Promise.all([
           request.server.methods.getPsoContactsByPolygon(polygon),
           request.server.methods.getFloodZoneByPolygon(polygon),
           isRiskAdminArea(polygon)]
         )
-
+        try {
+          const { payload } = await wreck.get(pauseP1URL, { json: true })
+          pauseP1DownloadFrom = new Date(payload.pauseP1DownloadFrom)
+          pauseP1DownloadTo = new Date(payload.pauseP1DownloadTo)
+          console.log(pauseP1DownloadFrom, pauseP1DownloadTo)
+        } catch (error) {
+          console.log('Error getting p1 pause', error)
+        }
+        const dateWithinPausePeriod = Date.now() >= pauseP1DownloadFrom.getTime() && Date.now() <= pauseP1DownloadTo.getTime()
+        console.log('dateWithinPausePeriod', dateWithinPausePeriod)
+        const p1UnpauseTimeAndDateString = `${pauseP1DownloadTo.toLocaleTimeString()} on the ${pauseP1DownloadTo.toLocaleDateString()}`
+        console.log('dateWithinPausePeriod', dateWithinPausePeriod)
         const showOrderProduct4Button = config.appType === 'internal' || contactData.useAutomatedService === true
         floodData.centreOfPolygon = getCentreOfPolygon(polygon)
         floodData.isRiskAdminArea = isRiskAdmin
-        return h.view('next-steps', { floodData, contactData, showOrderProduct4Button, encodedPolygon, polygon })
+        return h.view('next-steps', { floodData, contactData, showOrderProduct4Button, encodedPolygon, polygon, dateWithinPausePeriod, p1UnpauseTimeAndDateString })
       }
     }
   }
--- server/routes/results.js
+++ server/routes/results.js
@@ -1,4 +1,6 @@
 const { config } = require('../../config')
+const pauseP1URL = config.functionAppUrl + '/pause-p1-download'
+const wreck = require('@hapi/wreck')
 const {
   getAreaInHectares,
   getCentreOfPolygon,
@@ -12,11 +14,24 @@ module.exports = [
     options: {
       description: 'Results Page',
       handler: async (request, h) => {
+        let pauseP1DownloadFrom, pauseP1DownloadTo
         const { polygon, encodedPolygon } = checkParamsForPolygon(request.query)
         const [contactData, floodData] = await Promise.all([
           request.server.methods.getPsoContactsByPolygon(polygon),
           request.server.methods.getFloodDataByPolygon(polygon)]
         )
+        try {
+          const { payload } = await wreck.get(pauseP1URL, { json: true })
+          pauseP1DownloadFrom = new Date(payload.pauseP1DownloadFrom)
+          pauseP1DownloadTo = new Date(payload.pauseP1DownloadTo)
+          console.log(pauseP1DownloadFrom, pauseP1DownloadTo)
+        } catch (error) {
+          console.log('Error getting p1 pause', error)
+        }
+        const dateWithinPausePeriod = Date.now() >= pauseP1DownloadFrom.getTime() && Date.now() <= pauseP1DownloadTo.getTime()
+        console.log('dateWithinPausePeriod', dateWithinPausePeriod)
+        const p1UnpauseTimeAndDateString = `${pauseP1DownloadTo.toLocaleTimeString()} on the ${pauseP1DownloadTo.toLocaleDateString()}`
+        console.log('dateWithinPausePeriod', dateWithinPausePeriod)
         const showOrderProduct4Button = config.appType === 'internal' || contactData.useAutomatedService === true
         floodData.areaInHectares = getAreaInHectares(polygon)
         floodData.centreOfPolygon = getCentreOfPolygon(polygon)
@@ -24,7 +39,7 @@ module.exports = [
         floodData.isFZ1Andgt1ha = floodData.floodZone === '1' && floodData.areaInHectares >= 1
         floodData.areaInHectares = floodData.areaInHectares !== '0' && floodData.areaInHectares !== 0 ? floodData.areaInHectares : 'less than 0.01'
         floodData.riversAndSea = floodData.floodZone !== '1' || floodData.floodZoneClimateChange || floodData.floodZoneClimateChangeNoData
-        return h.view('results', { floodData, contactData, showOrderProduct4Button, encodedPolygon, polygon })
+        return h.view('results', { floodData, contactData, showOrderProduct4Button, encodedPolygon, polygon, dateWithinPausePeriod, p1UnpauseTimeAndDateString })
       }
     }
   }
