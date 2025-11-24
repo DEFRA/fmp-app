03bc25022b56a76dc2f2798895e5cb6820605eb5
Mark Fee <mark.fee@defra.gov.uk>
2025-11-24 16:30:50 +0000

    FCRM-6508 made all maps master-maps & removed the open maps

--- server/routes/__tests__/map-styles.spec.js
+++ server/routes/__tests__/map-styles.spec.js
@@ -1,10 +1,7 @@
 const { submitGetRequest } = require('../../__test-helpers__/server')
-const baseMapDefault = require('../defra-map/styles/OS_VTS_27700_Open_Outdoor.json')
-const baseMapDark = require('../defra-map/styles/OS_VTS_27700_Open_Dark.json')
-const baseMapBlackAndWhite = require('../defra-map/styles/OS_VTS_27700_Black_and_White.json')
-const baseMapBlackAndWhiteOpen = require('../defra-map/styles/OS_VTS_27700_Open_Black_and_White.json')
-const polygonDefault = require('../defra-map/styles/OS_VTS_27700_Outdoor.json')
-const polygonDark = require('../defra-map/styles/OS_VTS_27700_Dark.json')
+const blackAndWhiteMap = require('../defra-map/styles/OS_VTS_27700_Black_and_White.json')
+const masterMap = require('../defra-map/styles/OS_VTS_27700_Outdoor.json')
+const masterMapDark = require('../defra-map/styles/OS_VTS_27700_Dark.json')
 const openTile = require('../defra-map/styles/open-tile.json')
 const vtsTile = require('../defra-map/styles/vts-tile.json')
 
@@ -12,12 +9,10 @@ describe('map style routes', () => {
   const urls = [
     ['/map/styles/open-tile.json', openTile],
     ['/map/styles/vts-tile.json', vtsTile],
-    ['/map/styles/base-map-default', baseMapDefault],
-    ['/map/styles/base-map-dark', baseMapDark],
-    ['/map/styles/base-map-black-and-white', baseMapBlackAndWhite],
-    ['/map/styles/base-map-black-and-white-open', baseMapBlackAndWhiteOpen],
-    ['/map/styles/polygon-default', polygonDefault],
-    ['/map/styles/polygon-dark', polygonDark]
+
+    ['/map/styles/black-and-white-map', blackAndWhiteMap],
+    ['/map/styles/master-map', masterMap],
+    ['/map/styles/master-map-dark', masterMapDark]
   ]
 
   urls.forEach(([path, expectedResponse]) => {
