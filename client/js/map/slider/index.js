9cc3285ce23f627dd57089d2232f10cf2fe2d503
Mark Fee <mark.fee@defra.gov.uk>
2026-01-16 11:14:04 +0000

    FCRM-6539 fixed issue that caused the opacity slider to become detached from the react DOM (#686)
    * FCRM-6539 fixed issue that caused the opacity slider to become detached from the react dom
    
    * FCRM-6539 Addressed some Sonar Cloud issues

--- client/js/map/slider/index.js
+++ client/js/map/slider/index.js
@@ -1,41 +1,23 @@
-import sliderMarkUp from './slider.html.js'
 import { OpacitySlider } from './slider.js'
-const id = 'opacity-control'
+export { sliderMarkUp } from './slider.html.js'
+const containerId = 'opacity-control'
 let opacitySlider
 
-const observer = new window.MutationObserver((mutations) => {
-  // addedNodes is an array of NodeLists, which is an array of Nodes
-  const addedNodes = [...mutations.map(({ addedNodes: sonarAddedNodes }) => sonarAddedNodes)]
-  const sliderElement = addedNodes.reduce((sliderNode, nodeList) => {
-    sliderNode = sliderNode || [...nodeList].reduce((childSliderNode, node) => {
-      childSliderNode = childSliderNode || node.querySelector('#opacity-control')
-      return childSliderNode
-    }, null)
-    return sliderNode
-  }, null)
-  if (sliderElement) {
-    createOpacitySlider(sliderElement)
+const mapContainerObserver = new globalThis.MutationObserver((mutations) => {
+  const addedNodes = mutations.map(({ addedNodes: newNodes }) => newNodes)
+  if (addedNodes.length) {
+    opacitySlider.checkAndAttach()
   }
 })
 
-const createOpacitySlider = (sliderElement) => {
-  opacitySlider = new OpacitySlider(sliderElement)
-  opacitySlider.init()
-}
-
 const initialiseSlider = () => {
-  const sliderElement = document.getElementById(id)
+  opacitySlider = new OpacitySlider(containerId)
+  opacitySlider.checkAndAttach()
 
-  let opacityContainer = '.fm-o-side' // On non-mobile sliderElement is in the fm-o-side element
-  if (sliderElement) { // On the desktop, the element should exist
-    createOpacitySlider(sliderElement)
-  } else { // On mobile sliderElement is in the fm-o-middle element and won't appear until the layers menu is shown
-    opacityContainer = '.fm-o-middle'
-  }
-  // Now create an observer that will re-create the slider every time it goes away.
-  // EG On Mobile when the layers menu is shown, on desktop when edit mode is exited.
-  const mapOpacityContainer = document.querySelector(opacityContainer)
-  observer.observe(mapOpacityContainer, { attributes: false, childList: true, characterData: false, subtree: false })
+  // Observe the container for addedNodes - ie when fm-o-side appears on browser resize from tablet => desktop
+  const mapContainer = document.querySelector('.fm-o-container')
+  mapContainerObserver.observe(mapContainer, { attributes: false, childList: true, characterData: false, subtree: true })
+  return opacitySlider
 }
 
-export { sliderMarkUp, initialiseSlider }
+export { initialiseSlider }
