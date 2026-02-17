efc414ebec2baba1f32a6f469f84a9d92e7f9a25
Mark Fee <mark.fee@defra.gov.uk>
2026-02-17 12:37:18 +0000

    FCRM-6636 client side p1 WIP - shows reference values and Notes so far

--- server/routes/product-1.js
+++ server/routes/product-1.js
@@ -1,6 +1,11 @@
 const Boom = require('@hapi/boom')
 const Joi = require('joi')
 const { getProduct1 } = require('../services/eaMaps/getProduct1')
+const { formatUKDateTimeToMinute } = require('@defra/fmp-utilities/dates')
+const {
+  getCentreOfPolygon,
+  checkParamsForPolygon
+} = require('../services/shape-utils')
 
 const MAX_REFERENCE_WIDTH = 25
 const SCALE_2500 = 2500
@@ -8,7 +13,31 @@ const SCALE_10000 = 10000
 const SCALE_25000 = 25000
 const SCALE_50000 = 50000
 
-module.exports = {
+module.exports = [{
+  method: 'GET',
+  path: '/product-1',
+  options: {
+    description: 'Get Product 1 PDF',
+    handler: async (request, h) => {
+      const { scale, reference, floodZone } = request.query
+      const { polygon } = checkParamsForPolygon(request.query)
+      const isRiskAdminArea = request.query.isRiskAdminArea === 'true'
+      const createdDate = formatUKDateTimeToMinute(Date.now())
+      return h.view('product-1', { polygon, scale, reference, isRiskAdminArea, floodZone, createdDate })
+    },
+    validate: {
+      query: Joi.object().keys({
+        reference: Joi.string().allow('').max(MAX_REFERENCE_WIDTH).trim(),
+        scale: Joi.number().valid(SCALE_2500, SCALE_10000, SCALE_25000, SCALE_50000).default(SCALE_2500),
+        isRiskAdminArea: Joi.string().valid('true', 'false').required(),
+        floodZone: Joi.string().valid('1', '2', '3').required(),
+        encodedPolygon: Joi.string(),
+        polygon: Joi.string()
+      })
+    }
+  }
+},
+{
   method: 'POST',
   path: '/product-1',
   options: {
@@ -47,4 +76,4 @@ module.exports = {
       })
     }
   }
-}
+}]
