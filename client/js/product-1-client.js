d22134c616908cc065e05a69c561fa8909ac0063
Mark Fee <mark.fee@defra.gov.uk>
2026-02-23 12:17:23 +0000

    FCRM-6636 showing FZs - todo proxy via fmp-app, change layer order

--- client/js/product-1-client.js
+++ client/js/product-1-client.js
@@ -1,7 +1,29 @@
 import { showMap, convertToImage } from './static-map.js'
+import { getDefraMapConfig } from './map/tokens.js'
+import VectorTileLayer from '@arcgis/core/layers/VectorTileLayer.js'
+
+let layerNameSuffix, agolVectorTileUrl
+
+const addLayerToMap = (view, layerName) => {
+  const url = `${agolVectorTileUrl}/${layerName + layerNameSuffix}/VectorTileServer`
+  const vectorTileLayer = new VectorTileLayer({
+    id: layerName,
+    url,
+    opacity: 0.75,
+    visible: true
+  })
+  view.map.add(vectorTileLayer)
+  return view
+}
 
 const showMapAsImage = async (polygonArray) => {
+  await getDefraMapConfig().then((defraMapConfig) => {
+    layerNameSuffix = defraMapConfig.layerNameSuffix
+    agolVectorTileUrl = defraMapConfig.agolVectorTileUrl
+  })
+  console.log(layerNameSuffix)
   return showMap(polygonArray)
+    .then((view) => addLayerToMap(view, 'Flood_Zones_2_and_3_Rivers_and_Sea'))
     .then(convertToImage)
     .then((view) => {
       // Destroy the map to free up resources
