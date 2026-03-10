e15f66bc22021853989db66809ea2c213aad7f58
RyonLeightonDEFRA <ryon.leighton@defra.gov.uk>
2026-03-10 11:42:26 +0000

    remove old tests, linting fixes and config update

--- e2e/utils/decode-polygon.js
+++ e2e/utils/decode-polygon.js
@@ -3,7 +3,7 @@ import polyline from '@mapbox/polyline'
 /*
   TO Use this - from the command line in the project root type:
   node utils/decodePolygon.js 'ogt}qzhAomugdxO~ja{MopojGo~wd@_eznN_|hnJ_auiBon~eAnx`eZ'
-  -- where 'ogt}qzhAomugdxO~ja{MopojGo~wd@_eznN_|hnJ_auiBon~eAnx`eZ' is the encodedPolygon parameter from the querystring 
+  -- where 'ogt}qzhAomugdxO~ja{MopojGo~wd@_eznN_|hnJ_auiBon~eAnx`eZ' is the encodedPolygon parameter from the querystring
   - It must be in quotes.
 */
 
--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,22 @@
 module.exports = require('neostandard')({
   env: ['jest', 'node'],
   ignores: [
+    '**/_results_/',
     '**/defra-map/',
     '**/server/public/'
+  ],
+  overrides: [
+    {
+      files: 'e2e/**',
+      env: {
+        browser: true
+      },
+      globals: {
+        browser: 'readonly',
+        $: 'readonly',
+        $$: 'readonly',
+        baseUrl: 'readonly'
+      }
+    }
   ]
 })
