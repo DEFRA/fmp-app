968e9a0c44ad4cbe4f1042f9f3242de6fb39ea08
Mark Fee <mark.fee@defra.gov.uk>
2026-03-11 12:33:37 +0000

    FCRM-6736 polygon in url is updated on done

--- client/js/map/interactive-map-helpers/polygonFeature.js
+++ client/js/map/interactive-map-helpers/polygonFeature.js
@@ -2,13 +2,17 @@ import { getQueryParam, setQueryParam } from './queryParams.js'
 import { checkParamsForPolygon, encodePolygon } from '../../../../server/services/shape-utils.js'
 
 const getPolygonFromUrl = () => {
-  const encodedPolygon = getQueryParam('encodedPolygon')
-  const queryStringPolygon = getQueryParam('polygon')
-  if (!(encodedPolygon || queryStringPolygon)) {
+  try {
+    const encodedPolygon = getQueryParam('encodedPolygon')
+    const queryStringPolygon = getQueryParam('polygon')
+    if (!(encodedPolygon || queryStringPolygon)) {
+      return null
+    }
+    const { polygon: polygonString } = checkParamsForPolygon({ encodedPolygon, polygon: queryStringPolygon, encode: false })
+    return [JSON.parse(polygonString)]
+  } catch (_error) {
     return null
   }
-  const { polygon: polygonString } = checkParamsForPolygon({ encodedPolygon, polygon: queryStringPolygon, encode: false })
-  return JSON.parse(polygonString)
 }
 
 class PolygonFeature {
@@ -25,6 +29,9 @@ class PolygonFeature {
 
   set feature (feature) {
     this._feature = feature ? { ...feature, id: this._id } : null
+    const encodedPolygon = this._feature ? encodePolygon(this.coordinates[0]) : null
+    setQueryParam('encodedPolygon', encodedPolygon)
+    setQueryParam('polygon', null)
   }
 
   get coordinates () {
@@ -32,13 +39,17 @@ class PolygonFeature {
   }
 
   set coordinates (coordinates) {
-    this._feature = {
-      id: this._id,
-      type: 'feature',
-      geometry: {
-        type: 'Polygon',
-        coordinates
+    if (coordinates) {
+      this._feature = {
+        id: this._id,
+        type: 'feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates
+        }
       }
+    } else {
+      this.feature = null
     }
   }
 }
--- client/js/map/interactive-map-helpers/queryParams.js
+++ client/js/map/interactive-map-helpers/queryParams.js
@@ -14,5 +14,6 @@ export const setQueryParam = (key, value) => {
   }
 
   // Update the URL without reloading
+  url.search = decodeURIComponent(url.search)
   window.history.replaceState({}, '', url.toString())
 }
