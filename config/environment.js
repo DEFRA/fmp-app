c02f108a2692161c62f42105ae51debd439b2774
RyonLeightonDEFRA <ryon.leighton@defra.gov.uk>
2026-03-19 08:21:55 +0000

    removing extra tests and logic

--- config/environment.js
+++ config/environment.js
@@ -1,9 +1,1 @@
-const fs = require('fs')
-const path = require('path')
-
-const envPath = path.resolve(__dirname, '..', '.env')
-const examplePath = path.resolve(__dirname, '.env-example')
-
-require('dotenv').config({
-  path: fs.existsSync(envPath) ? envPath : examplePath
-})
+require('dotenv').config()
