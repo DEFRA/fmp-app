2947f746732d2cbc853993a2cf178f437b083c28
Michael Steinacher <steinacherm@hotmail.co.uk>
2026-04-01 15:38:51 +0100

    Fixed tests.

--- server/services/agol/getFloodZones.js
+++ server/services/agol/getFloodZones.js
@@ -5,9 +5,10 @@ const assignFloodZoneResponse = (response, results) => {
   for (const { attributes } of response) {
     results.floodzone_2 = results.floodzone_2 || (attributes.flood_zone === 'FZ2')
     results.floodzone_3 = results.floodzone_3 || (attributes.flood_zone === 'FZ3')
-    results.hasRiverSource = results.hasRiverSource || attributes.flood_source === 'river'
+    results.hasRiversSource = results.hasRiversSource || attributes.flood_source === 'river'
     results.hasSeaSource = results.hasSeaSource || attributes.flood_source === 'sea'
-    if (results.floodzone_2 && results.floodzone_3 && results.hasRiverSource && results.hasSeaSource) {
+    results.hasRiversAndSeaSource = results.hasRiversAndSeaSource || attributes.flood_source === 'river and sea'
+    if ((results.floodzone_2 && results.floodzone_3) && ((results.hasRiversSource && results.hasSeaSource) || results.hasRiversAndSeaSource)) {
       break // We can stop early once we find FZs 2, 3, river and sea sources
     }
   }
@@ -22,14 +23,16 @@ const assignFloodZoneResponse = (response, results) => {
     results.floodZoneLevel = 'low'
   }
 
-  if (results.hasRiverSource && results.hasSeaSource) {
-    results.hasRiversAndSeaSource = 'river and sea'
+  if ((results.hasRiversSource && results.hasSeaSource) || results.hasRiversAndSeaSource) {
+    results.hasRiversAndSeaSource = true
   }
-  results.floodSource = results.hasRiverSource && results.hasSeaSource
-    ? 'rivers and the sea' : results.hasRiverSource
-      ? 'rivers' : results.hasSeaSource
-        ? 'the sea' : null
-  console.log('AGOL flood zone results', results)
+  results.floodSource = (results.hasRiversSource && results.hasSeaSource) || results.hasRiversAndSeaSource
+    ? 'rivers and the sea'
+    : results.hasRiversSource
+      ? 'rivers'
+      : results.hasSeaSource
+        ? 'the sea'
+        : null
   return results
 }
 
@@ -38,7 +41,8 @@ const getFloodZones = async (options) => {
     floodzone_2: false,
     floodzone_3: false,
     hasSeaSource: false,
-    hasRiverSource: false,
+    hasRiversSource: false,
+    hasRiversAndSeaSource: false,
     floodSource: null,
   }
 
