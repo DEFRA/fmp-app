ea7de5642ac0226ad57e7dd346708099e11132db
RyonLeightonDEFRA <ryon.leighton@defra.gov.uk>
2026-06-12 11:47:50 +0100

    lint fix

--- e2e/tests/pages/map-page.spec.js
+++ e2e/tests/pages/map-page.spec.js
@@ -44,56 +44,56 @@ test.describe('Map page', () => {
     }
   })
 
-test.describe('surface water dataset', () => {
-  test.beforeEach(async ({ mapSteps }) => {
-    await mapSteps.expandSection(pages.map.datasetsSection)
-    await mapSteps.chooseMenuOption(pages.map.surfaceWaterOption)
+  test.describe('surface water dataset', () => {
+    test.beforeEach(async ({ mapSteps }) => {
+      await mapSteps.expandSection(pages.map.datasetsSection)
+      await mapSteps.chooseMenuOption(pages.map.surfaceWaterOption)
+    })
+
+    test('updates map when selecting climate change options', async ({ mapSteps }) => {
+      await mapSteps.expandSection(pages.map.climateChangeSection)
+      await mapSteps.assertRadiosUpdateMap(pages.map.climateOptionsSW)
+    })
+
+    test('updates map when selecting annual likelihood options', async ({ mapSteps }) => {
+      await mapSteps.expandSection(pages.map.annualLikelihoodSection)
+      await mapSteps.assertRadiosUpdateMap(pages.map.annualLikelihoodOptions)
+    })
+
+    test('updates map when selecting depth options', async ({ mapSteps }) => {
+      await mapSteps.expandSection(pages.map.depthSection)
+      await mapSteps.assertRadiosUpdateMap(pages.map.surfaceWaterDepthOptions)
+    })
   })
 
-  test('updates map when selecting climate change options', async ({ mapSteps }) => {
-    await mapSteps.expandSection(pages.map.climateChangeSection)
-    await mapSteps.assertRadiosUpdateMap(pages.map.climateOptionsSW)
+  test.describe('map search', () => {
+    test.beforeEach(async ({ mapSteps }) => {
+      await mapSteps.openSearch()
+    })
+
+    test('shows no results for invalid search', async ({ mapSteps }) => {
+      await mapSteps.search('qzxwvvbnnmm112233445566778899')
+      await mapSteps.expectText('No results')
+    })
+
+    test('shows results and relocates map for valid search', async ({ mapSteps }) => {
+      const prevUrl = mapSteps.page.url()
+      await mapSteps.search('Leeds')
+      await mapSteps.selectSearchResult()
+      await mapSteps.expectUrlChanged(prevUrl)
+    })
   })
 
-  test('updates map when selecting annual likelihood options', async ({ mapSteps }) => {
-    await mapSteps.expandSection(pages.map.annualLikelihoodSection)
-    await mapSteps.assertRadiosUpdateMap(pages.map.annualLikelihoodOptions)
+    test('dismisses key panel and alert banner', async ({ mapSteps }) => {
+      await mapSteps.expectVisible('dialog', 'Key')
+      await mapSteps.dismissPanel('Key')
+      await mapSteps.expectHidden('dialog', 'Key')
+      await mapSteps.dismissBanner(/flood zone/i)
   })
 
-  test('updates map when selecting depth options', async ({ mapSteps }) => {
-    await mapSteps.expandSection(pages.map.depthSection)
-    await mapSteps.assertRadiosUpdateMap(pages.map.surfaceWaterDepthOptions)
-  })
-})
-
-test.describe('map search', () => {
-  test.beforeEach(async ({ mapSteps }) => {
-    await mapSteps.openSearch()
-  })
-
-  test('shows no results for invalid search', async ({ mapSteps }) => {
-    await mapSteps.search('qzxwvvbnnmm112233445566778899')
-    await mapSteps.expectText('No results')
-  })
-
-  test('shows results and relocates map for valid search', async ({ mapSteps }) => {
-    const prevUrl = mapSteps.page.url()
-    await mapSteps.search('Leeds')
-    await mapSteps.selectSearchResult()
-    await mapSteps.expectUrlChanged(prevUrl)
-  })
-})
-
-  test('dismisses key panel and alert banner', async ({ mapSteps }) => {
-    await mapSteps.expectVisible('dialog', 'Key')
-    await mapSteps.dismissPanel('Key')
-    await mapSteps.expectHidden('dialog', 'Key')
-    await mapSteps.dismissBanner(/flood zone/i)
-})
-
-  test('navigates to map help page', async ({ mapSteps }) => {
-    await mapSteps.clickLink(pages.map.helpLink)
-    await mapSteps.switchToNewWindow()
-    await mapSteps.expectOn(pages.mapHelp.page)
-  })
+    test('navigates to map help page', async ({ mapSteps }) => {
+      await mapSteps.clickLink(pages.map.helpLink)
+      await mapSteps.switchToNewWindow()
+      await mapSteps.expectOn(pages.mapHelp.page)
+    })
 })
