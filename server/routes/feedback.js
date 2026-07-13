39a4e53c351d59aa5d029476f4072d04ec1a6384
Michael Steinacher <steinacherm@hotmail.co.uk>
2026-07-13 14:04:19 +0100

    Added the href referrer safety check.

--- server/routes/feedback.js
+++ server/routes/feedback.js
@@ -1,4 +1,26 @@
 const constants = require('../constants')
+const { isSafeRedirect } = require('../common/utils/is-safe-redirect')
+
+function getSafeExitSurveyHref (request) {
+  const { referrer, host } = request.info
+  const { protocol } = request.server.info
+
+  if (!referrer || referrer.indexOf(constants.routes.FEEDBACK) > -1) {
+    return '/'
+  }
+
+  if (isSafeRedirect(referrer)) {
+    return referrer
+  }
+
+  try {
+    const referrerUrl = new URL(referrer, `${protocol}://${host}`)
+    const redirectUrl = `${referrerUrl.pathname}${referrerUrl.search}`
+    return isSafeRedirect(redirectUrl) ? redirectUrl : '/'
+  } catch {
+    return '/'
+  }
+}
 
 module.exports = {
   method: 'GET',
@@ -10,10 +32,11 @@ module.exports = {
         request.info.referrer && request.info.referrer.indexOf(constants.routes.FEEDBACK) === -1
           ? request.info.referrer
           : request.server.info.protocol + '://' + request.info.host
+
       return h.view(constants.views.FEEDBACK, {
         ref: encodeURIComponent(ref),
         userAgent: encodeURIComponent(request.headers['user-agent'] || ''),
-        href: ref
+        href: getSafeExitSurveyHref(request)
       })
     }
   }
