3f0179d64abbbe6782f9640bbff84fa771dc56c5
Mark Fee <mark.fee@defra.gov.uk>
2026-07-30 14:11:19 +0100

    FCRM-6990 replaced strings with terms

--- client/js/map/draw/drawPlugin.js
+++ client/js/map/draw/drawPlugin.js
@@ -2,6 +2,7 @@ import createDrawPlugin from '@defra/interactive-map/plugins/draw-es'
 import createFramePlugin from '@defra/interactive-map/plugins/frame'
 import { hideHelpPanel, showHelpPanel } from '../helpBanner.js'
 import { polygonFeature } from '../interactive-map-helpers/polygonFeature.js'
+import { terms } from '..//terms.js'
 
 export const drawPlugin = createDrawPlugin({
   onGeometryChange: (geometry) => true
@@ -45,13 +46,13 @@ export const attachDrawPlugin = (interactiveMap) => {
     polygonFeature.mapView = view
 
     interactiveMap.addButton('geometryActions', {
-      label: 'Get data for your location',
+      label: terms.labels.drawMenuTitle,
       mobile: { slot: 'bottom-right', order: 4 },
       tablet: { slot: 'top-left', order: 4 },
       desktop: { slot: 'top-left', order: 4 },
       menuItems: [{
         id: 'addPolygon',
-        label: 'Add polygon',
+        label: terms.labels.addPolygon,
         iconSvgContent: '<path d="M19.5 7v10M4.5 7v10M7 19.5h10M7 4.5h10"/><path d="M22 18v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zm0-15v3a1 1 0 0 1-1 1h-3a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zM7 18v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1v-3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1zM7 3v3a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h3a1 1 0 0 1 1 1z"/>',
         onClick: function (e) {
           drawPlugin.newPolygon(polygonFeature.id)
@@ -61,7 +62,7 @@ export const attachDrawPlugin = (interactiveMap) => {
         }
       }, {
         id: 'addSquare',
-        label: 'Add square',
+        label: terms.labels.addSquare,
         iconSvgContent: '<rect width="18" height="18" x="3" y="3" rx="2"/>',
         onClick: function (e) {
           framePlugin.addFrame(polygonFeature.id, { aspectRatio: 1 })
@@ -72,7 +73,7 @@ export const attachDrawPlugin = (interactiveMap) => {
       },
       {
         id: 'uploadShape',
-        label: 'Upload a boundary',
+        label: terms.labels.uploadShape,
         iconSvgContent: '<line x1="12" y1="3" x2="12" y2="15" stroke="currentColor" stroke-width="2" stroke-linecap="round"/><polyline points="7,8 12,3 17,8" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><polyline points="5,17 5,19 19,19 19,17" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>',
         isDisabled: false,
         onClick: () => {
@@ -81,7 +82,7 @@ export const attachDrawPlugin = (interactiveMap) => {
       },
       {
         id: 'editShape',
-        label: 'Edit feature',
+        label: terms.labels.editShape,
         iconSvgContent: '<path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/>',
         isDisabled: true,
         onClick: function () {
@@ -97,7 +98,7 @@ export const attachDrawPlugin = (interactiveMap) => {
       },
       {
         id: 'deleteShape',
-        label: 'Delete shape',
+        label: terms.labels.deleteShape,
         iconSvgContent: '<path d="M10 11v6"/><path d="M14 11v6"/><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6"/><path d="M3 6h18"/><path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"/>',
         isDisabled: true,
         onClick: () => {
