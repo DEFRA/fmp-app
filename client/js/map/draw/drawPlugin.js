4738d5ab0c7569a58a3a316b3e74e816bffd1b8e
Mark Fee <mark.fee@defra.gov.uk>
2026-07-30 11:29:09 +0100

    FCRM-6990 added upload to draw menu

--- client/js/map/draw/drawPlugin.js
+++ client/js/map/draw/drawPlugin.js
@@ -81,6 +81,15 @@ export const attachDrawPlugin = (interactiveMap) => {
         }
       },
       {
+        id: 'uploadShape',
+        label: 'Upload a boundary',
+        iconSvgContent: '<line x1="12" y1="3" x2="12" y2="15" stroke="currentColor" stroke-width="2" stroke-linecap="round"/><polyline points="7,8 12,3 17,8" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/><polyline points="5,17 5,19 19,19 19,17" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>',
+        isDisabled: false,
+        onClick: () => {
+          globalThis.location.href = '/upload'
+        }
+      },
+      {
         id: 'editShape',
         label: 'Edit feature',
         iconSvgContent: '<path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z"/><path d="m15 5 4 4"/>',
