5c60cd8ae732d1909120396543106a6a105ca103
Mark Fee <mark.fee@defra.gov.uk>
2026-08-03 17:47:26 +0100

    FCRM-6990 - added the draw plugin - to digitise a polygon (#829)
    * FCRM-6990 can add/delete/edit/cancel polygons/frames
    
    * FCRM-6990 help banner and menu shows and hides when editing a shape
    
    * FCRM-6990  zoom in when editing a frame (square) - so default sizes are small
    
    * FCRM-6990 removed min-release-age as it conflicted with builds
    
    * FCRM-6990 removed duplicated code after rebase
    
    * FCRM-6990 added upload to draw menu
    
    * FCRM-6990 refactored drawPlugin to use polygonFeature
    
    * FCRM-6990 replaced strings with terms
    
    * FCRM-6990 Only show info panel when a feature is clicked
    
    * FCRM-6990 Some Sonar issues addressed
    
    * FCRM-6990 reduce function complexity
    
    * FCRM-6990 further 'simplified' menu
    
    * FCRM-6990 removed console.log of hovered layer
    
    * FCRM-6990 renamed polygonFeature as siteBoundary
    
    * FCRM-6990 removed menu.js
    
    * removed key.js
    
    * FCRM-6990 moved get extents to siteBoundary class
    
    * FCRM-6990 added a round polygon to siteBoundary when url is populated
    
    * FCRM-6990 fixed some not at all major, major issues
    
    * FCRM-6990 added an onEditPolygon callback
    
    * FCRM-6990 dataset layers and key hide when editing

--- client/js/map/index.js
+++ client/js/map/index.js
@@ -9,46 +9,29 @@ import { interactPlugin, attachInteractPlugin } from './interactive-map-helpers/
 
 import { setupEsriConfig, getRequest, getDefraMapConfig } from './tokens.js'
 import { setUpBaseMaps } from './baseMap.js'
-import { checkParamsForPolygon, encodePolygon } from '../../../server/services/shape-utils.js'
+// import { encodePolygon } from '../../../server/services/shape-utils.js'
+import { siteBoundary } from './interactive-map-helpers/siteBoundary.js'
 // TODO: add the slider to the dataset plugin
 // import { sliderMarkUp, initialiseSlider } from './slider/index.js'
 import { getInfoPanel } from './infoPanel.js'
 
 // <InteractiveMapHelpers>
-import { datasetsPlugin } from './datasets/datasetsPlugin.js'
-import { drawPlugin, framePlugin, attachDrawPluginHandlers } from './interactive-map-helpers/draw.js'
-// </InteractiveMapHelpers>
+import { initialiseDatasetsPlugin } from './datasets/datasetsPlugin.js'
 
-const mapDiv = document.getElementById('map')
+import { drawPlugin, framePlugin, attachDrawPlugin } from './draw/drawPlugin.js'
+
+import { addHelpBanner, hideHelpPanel, showHelpPanel } from './helpBanner.js'
+
+const ENGLAND_WEST = 50000
+const ENGLAND_SOUTH = 10000
+const ENGLAND_EAST = 400000
+const ENGLAND_NORTH = 650000
 
 const symbols = {
   noData: '/assets/images/no-data.svg',
   waterStorageAreas: '/assets/images/water-storage.svg',
   floodDefences: '/assets/images/flood-defence.svg',
   mainRivers: '/assets/images/main-rivers.svg'
-}
-
-// const MAX_POLYGON_AREA = 3000000
-
-// capture polygon from query string
-const queryParams = new URLSearchParams(window.location.search)
-
-const calculateExtent = (polygonToCalculate) => {
-  const calculatedExtent = polygonToCalculate.reduce((acc, [x, y]) => {
-    acc[0] = Math.min(acc[0], x)
-    acc[1] = Math.min(acc[1], y)
-    acc[2] = Math.max(acc[2], x)
-    acc[3] = Math.max(acc[3], y)
-    return acc
-  }, [Infinity, Infinity, -Infinity, -Infinity])
-  return calculatedExtent
-}
-
-let extent
-if (queryParams.get('encodedPolygon') || queryParams.get('polygon')) {
-  const { polygon: polygonString } = checkParamsForPolygon({ encodedPolygon: queryParams.get('encodedPolygon'), polygon: queryParams.get('polygon'), encode: false })
-  const polygon = JSON.parse(polygonString)
-  extent = calculateExtent(polygon)
 }
 
 getDefraMapConfig().then((defraMapConfig) => {
@@ -73,13 +56,14 @@ getDefraMapConfig().then((defraMapConfig) => {
       panels: [mapStylePanelOverrides]
     }
   })
+  const datasetsPlugin = initialiseDatasetsPlugin(defraMapConfig)
 
   const interactiveMap = new InteractiveMap('map', {
     mapProvider: esriProvider({
       setupConfig: setupEsriConfig
     }),
     plugins: [
-      datasetsPlugin(defraMapConfig),
+      datasetsPlugin,
       mapStylePlugin,
       createScaleBarPlugin({ units: 'metric' }),
       createSearchPlugin({
@@ -88,8 +72,7 @@ getDefraMapConfig().then((defraMapConfig) => {
         osNamesURL: 'https://api.os.uk/search/names/v1/find?query={query}&fq=local_type:postcode%20local_type:hamlet%20local_type:village%20local_type:town%20local_type:city%20local_type:suburban_area%20local_type:other_settlement&maxresults=8',
         regions: ['england'],
         width: '300px',
-        showMarker: true,
-        // expanded: true
+        showMarker: true
       }),
       drawPlugin,
       framePlugin,
@@ -99,7 +82,7 @@ getDefraMapConfig().then((defraMapConfig) => {
     place: 'England',
     minZoom: 6,
     maxZoom: 20,
-    extent: extent || [50000, 10000, 400000, 650000],
+    extent: siteBoundary.extents || [ENGLAND_WEST, ENGLAND_SOUTH, ENGLAND_EAST, ENGLAND_NORTH],
     containerHeight: '100%',
     enableZoomControls: true,
     symbols: [symbols.waterStorageAreas, symbols.floodDefences, symbols.mainRivers, symbols.noData],
@@ -119,8 +102,44 @@ getDefraMapConfig().then((defraMapConfig) => {
       reported = true
     }
   }
-  attachDrawPluginHandlers(interactiveMap)
+  let datasetsKeyExpanded = false
+  const toggleKeyWhenEditing = (isEditing) => {
+    const datasetsKey = document.getElementById('map-datasets-key')
+    if (isEditing) {
+      datasetsKeyExpanded = (datasetsKey?.getAttribute('aria-expanded') === 'true')
+      if (datasetsKeyExpanded) {
+        interactiveMap.hidePanel('datasetsKey')
+      }
+      datasetsKey.parentNode.style.display = 'none'
+    } else {
+      if (datasetsKeyExpanded) { // reinstate key
+        interactiveMap.showPanel('datasetsKey')
+      }
+      datasetsKey.parentNode.style.display = ''
+    }
+  }
+
+  const onEditPolygon = (isEditing) => {
+    toggleKeyWhenEditing(isEditing)
+    if (isEditing) {
+      interactiveMap.removePanel('info')
+      interactiveMap.removeMarker('search')
+      interactiveMap.hidePanel('datasetsLayers')
+      hideHelpPanel()
+      if (datasetsPlugin.ready) { // hide layers
+        datasetsPlugin.setDatasetVisibility(false)
+      }
+    } else {
+      showHelpPanel() // Only Shows it if the user has not dismissed it before
+      interactiveMap.showPanel('datasetsLayers')
+      if (datasetsPlugin.ready) { // reinstate layers
+        datasetsPlugin.setDatasetVisibility(true)
+      }
+    }
+  }
+
   attachInteractPlugin(interactiveMap)
+  attachDrawPlugin(interactiveMap, onEditPolygon)
 
   interactiveMap.on('app:ready', function (e) {
     interactiveMap.addButton('help', {
@@ -136,6 +155,7 @@ getDefraMapConfig().then((defraMapConfig) => {
   })
 
   interactiveMap.on('datasets:ready', function () {
+    datasetsPlugin.ready = true
     updateVisibleLayers()
     initPointerMove()
   })
@@ -143,96 +163,23 @@ getDefraMapConfig().then((defraMapConfig) => {
   interactiveMap.on('map:ready', function ({ map, view, _mapStyleId, _mapSize, _crs }) {
     mapState.map = map
     mapState.view = view
-
-    interactiveMap.addPanel('help-banner', {
-      label: 'Click on the flood zones for information',
-      html: '<span class="im-u-visually-hidden">Alert:</span>',
-      mobile: { slot: 'banner', dismissible: true },
-      tablet: { slot: 'banner', dismissible: true, width: '372px' },
-      desktop: { slot: 'banner', dismissible: true, width: '372px' }
-    })
+    addHelpBanner(interactiveMap)
   })
 
   interactiveMap.on('interact:markerchange', function (e) {
-    interactiveMap.addPanel('info', {
-      label: 'Info',
-      html: '<p>Some info</p>',
-      visibleGeometry: { type: 'Feature', geometry: { type: 'Point', coordinates: e.coords } }
-    })
-  })
-  let reported = false
-  interactiveMap.addEventListener = () => {
-    if (!reported) {
-      console.log('TODO - fix the listeners')
-      reported = true
+    if (mapState.cursorStyleLayer) {
+      interactiveMap.addPanel('info', {
+        label: 'Info',
+        html: `<div>
+            <p>Some info:</p>
+            <p>${mapState.cursorStyleLayer}</p>
+          </div>`,
+        visibleGeometry: { type: 'Feature', geometry: { type: 'Point', coordinates: e.coords } }
+      })
+    } else {
+      interactiveMap.removeMarker('infoPanelMarker')
+      interactiveMap.removePanel('info')
     }
-  }
-  attachLayers(interactiveMap, defraMapConfig)
-  addFeatureLayers(interactiveMap, defraMapConfig)
-  attachKeyHandlers(interactiveMap)
-  attachDrawPluginHandlers(interactiveMap)
-  attachInteractPlugin(interactiveMap)
-
-  interactiveMap.on('app:ready', function (e) {
-    interactiveMap.addButton('help', {
-      label: 'Help',
-      href: '/map-help',
-      iconSvgContent: '<circle cx="12" cy="12" r="10"/><path d="M9.09 9a3 3 0 0 1 5.83 1c0 2-3 3-3 3"/><path d="M12 17h.01"/>',
-      mobile: { slot: 'right-top', showLabel: false },
-      tablet: { slot: 'right-top', showLabel: false, order: 1 },
-      desktop: { slot: 'right-top', showLabel: true, order: 1 }
-    })
-    interactiveMap.addButton('menu', {
-      label: 'Menu',
-      panelId: 'menu',
-      iconSvgContent: '<path d="M12.83 2.18a2 2 0 0 0-1.66 0L2.6 6.08a1 1 0 0 0 0 1.83l8.58 3.91a2 2 0 0 0 1.66 0l8.58-3.9a1 1 0 0 0 0-1.83z"/><path d="M2 12a1 1 0 0 0 .58.91l8.6 3.91a2 2 0 0 0 1.65 0l8.58-3.9A1 1 0 0 0 22 12"/><path d="M2 17a1 1 0 0 0 .58.91l8.6 3.91a2 2 0 0 0 1.65 0l8.58-3.9A1 1 0 0 0 22 17"/>',
-      mobile: { slot: 'top-left', order: 1, showLabel: false },
-      tablet: { slot: 'top-left', order: 2 },
-      desktop: { slot: 'top-left', order: 2 }
-    })
-    interactiveMap.addButton('key', {
-      label: 'Key',
-      panelId: 'key',
-      iconSvgContent: '<path d="M3 5h.01"/><path d="M3 12h.01"/><path d="M3 19h.01"/><path d="M8 5h13"/><path d="M8 12h13"/><path d="M8 19h13"/>',
-      mobile: { slot: 'top-left', order: 2, showLabel: false },
-      tablet: { slot: 'top-left', order: 3 },
-      desktop: { slot: 'top-left', order: 3 }
-    })
-    interactiveMap.addPanel('menu', {
-      label: 'Menu',
-      html: renderMenuHTML(feature),
-      mobile: { slot: 'side', modal: true, open: false, dismissible: true },
-      tablet: { slot: 'side', width: '260px', open: true, dismissible: true },
-      desktop: { slot: 'side', width: '280px', open: true, dismissible: false }
-    })
-    interactiveMap.addPanel('key', {
-      label: 'Key',
-      html: renderKeyHTML(),
-      mobile: { slot: 'drawer', open: false, exclusive: true },
-      tablet: { slot: 'left-top', width: '260px', open: false, exclusive: true },
-      desktop: { slot: 'left-top', width: '280px', open: true, exclusive: false }
-    })
-    initialiseSlider(interactiveMap)
-  })
-
-  interactiveMap.on('map:ready', function ({ map, view, mapStyleId, mapSize, crs }) {
-    initPointerMove(view)
-
-    interactiveMap.addPanel('help-banner', {
-      label: 'Click on the flood zones for information',
-      html: '<span class="im-u-visually-hidden">Alert:</span>',
-      mobile: { slot: 'banner', dismissible: true },
-      tablet: { slot: 'banner', dismissible: true, width: '372px' },
-      desktop: { slot: 'banner', dismissible: true, width: '372px' }
-    })
-  })
-
-  interactiveMap.on('interact:markerchange', function (e) {
-    interactiveMap.addPanel('info', {
-      label: 'Info',
-      html: '<p>Some info</p>',
-      visibleGeometry: { type: 'Feature', geometry: { type: 'Point', coordinates: e.coords } }
-    })
   })
 
   const mapState = {
@@ -251,7 +198,6 @@ getDefraMapConfig().then((defraMapConfig) => {
       item.visible === true &&
       item.id !== 'baselayer'
     )
-    console.log('visibleLayers', mapState.visibleLayers)
   }
 
   const assignCursorStyleLayer = (hitTestResponse) => {
@@ -272,10 +218,7 @@ getDefraMapConfig().then((defraMapConfig) => {
 
       topVisibleStyleLayerId = visibleStyleLayerIds?.[0] || null
     }
-    if (mapState.cursorStyleLayer !== topVisibleStyleLayerId) {
-      mapState.cursorStyleLayer = topVisibleStyleLayerId
-      console.log('cursorStyleLayer', mapState.cursorStyleLayer)
-    }
+    mapState.cursorStyleLayer = topVisibleStyleLayerId
     document.body.style.cursor = mapState.cursorStyleLayer ? 'pointer' : 'default'
   }
 
@@ -301,38 +244,6 @@ getDefraMapConfig().then((defraMapConfig) => {
       mapState.visibleLayers = null
     })
   }
-
-  mapDiv.addEventListener('appaction', e => {
-    const { type } = e.detail
-    if (type === 'confirmPolygon' || type === 'updatePolygon') {
-      const url = new URL(window.location)
-      const polygon = e.detail?.query?.geometry?.coordinates?.[0]
-      mapState.polygon = roundPolygon(polygon)
-      url.searchParams.set('encodedPolygon', encodePolygon(polygon))
-      url.search = decodeURIComponent(url.search)
-      window.history.replaceState(null, '', url)
-    }
-    if (type === 'deletePolygon') {
-      delete mapState.polygon
-      const url = new URL(window.location)
-      url.searchParams.delete('encodedPolygon')
-      url.search = decodeURIComponent(url.search)
-      window.history.replaceState(null, '', url)
-    }
-  })
-
-  const roundPolygon = (polygon) => {
-    return polygon.map(([x, y]) => [Math.round(x * 100) / 100, Math.round(y * 100) / 100])
-  }
-
-  // event to fire for 'Get site report' button to non dynamic results page
-  document.addEventListener('click', e => {
-    if (e.target.innerText === 'Get summary report') {
-      const polygon = mapState.polygon
-      const encodedPolygon = encodePolygon(polygon)
-      window.location = `/results?encodedPolygon=${encodedPolygon}`
-    }
-  })
 
   // Listen to map queries
   interactiveMap.addEventListener('query', async e => {
--- client/js/map/interactive-map-helpers/interact.js
+++ client/js/map/interactive-map-helpers/interact.js
@@ -1,6 +1,7 @@
 import createInteractPlugin from '@defra/interactive-map/plugins/interact'
 export const interactPlugin = createInteractPlugin({
   marker: {
+    id: 'infoPanelMarker',
     symbol: 'pin',
     backgroundColor: { outdoor: '#0b0c0c', dark: '#ffffff' },
     foregroundColor: { outdoor: '#ffffff', dark: '#0b0c0c' }
