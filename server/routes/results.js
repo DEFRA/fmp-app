48f022acc46fa707abd388131694507c9c311b95
Mark Fee <mark.fee@defra.gov.uk>
2026-08-07 13:58:00 +0100

    FCRM-6999 Fixed in england issue on results, when in Scotland/Wales

--- server/routes/results.js
+++ server/routes/results.js
@@ -2,6 +2,7 @@ const { config } = require('../../config')
 const constants = require('../constants')
 const pauseP1URL = config.functionAppUrl + '/product-one-config'
 const { getProductOnePause } = require('../services/getProductOnePause')
+const isEnglandService = require('../services/is-england')
 const {
   getAreaInHectares,
   getCentreOfPolygon,
@@ -19,6 +20,25 @@ const enhanceFloodData = (floodData, polygon) => {
   return floodData
 }
 
+const isInEngland = async (contactData, polygon) => {
+  if (contactData.isEngland) {
+    // isEngland is set to true in getPsoContactsByPolygon if there is an areaTeam
+    // So we can be confident that the location is in England if there is an areaTeam
+    return true
+  }
+  if (!contactData.LocalAuthorities) {
+    // If there is no local authority data AND no area team
+    // then we can be confident that the location is not in England - usually off the coast
+    return false
+  }
+  // The local authority could potentially be Welsh or Scottish
+  // so we need to do an is in england polygon check too if all else fails.
+  // If this returns true, then we know we are at least partially in England,
+  // and can proceed with the results page.
+  // where it will show a contact the EA notice, in place of order a product 4 button.
+  return isEnglandService.isPolygonInEngland(polygon)
+}
+
 module.exports = [
   {
     method: 'GET',
@@ -29,12 +49,9 @@ module.exports = [
         const { polygon, encodedPolygon } = checkParamsForPolygon(request.query)
         const [contactData, floodData] = await Promise.all([
           request.server.methods.getPsoContactsByPolygon(polygon),
-          request.server.methods.getFloodDataByPolygon(polygon)]
-        )
-        if (contactData.isEngland === false && !contactData.LocalAuthorities) {
-          // Only redirect to England only page if the location is not in England
-          // and there is no local authority data, otherwise show the results
-          // page with the EA national team contact data
+          request.server.methods.getFloodDataByPolygon(polygon)
+        ])
+        if (!await isInEngland(contactData, polygon)) {
           return h.redirect(`${constants.routes.ENGLAND_ONLY}`)
         }
         const pauseP1Data = await getProductOnePause(pauseP1URL)
--- server/services/__tests__/is-england.spec.js
+++ server/services/__tests__/is-england.spec.js
@@ -18,27 +18,57 @@ describe('is-england', () => {
     stopMockingEsriRequests()
   })
 
-  it('is-england without easting or northing should throw "No point provided"', async () => {
-    await expect(
-      isEnglandService.isEnglandService(undefined, undefined)
-    ).rejects.toThrow('No point provided')
+  describe('isEnglandService', () => {
+    it('is-england without easting or northing should throw "No point provided"', async () => {
+      await expect(
+        isEnglandService.isEnglandService(undefined, undefined)
+      ).rejects.toThrow('No point provided')
+    })
+
+    it('is-england without easting should throw "No point provided"', async () => {
+      await expect(
+        isEnglandService.isEnglandService(undefined, 388244)
+      ).rejects.toThrow('No point provided')
+    })
+
+    it('is-england without northing should throw "No point provided"', async () => {
+      await expect(
+        isEnglandService.isEnglandService(388244, undefined)
+      ).rejects.toThrow('No point provided')
+    })
+
+    it('is-england with northing and easting should call esriFeatureRequest"', async () => {
+      const point = { northing: 388244, easting: 388244 }
+      const response = await isEnglandService.isEnglandService(point.easting, point.northing)
+      expect(response).toEqual(true)
+    })
   })
 
-  it('is-england without easting should throw "No point provided"', async () => {
-    await expect(
-      isEnglandService.isEnglandService(undefined, 388244)
-    ).rejects.toThrow('No point provided')
-  })
+  describe('isPolygonInEngland', () => {
+    const polygon = [[0, 0], [0, 1], [1, 1], [1, 0], [0, 0]]
 
-  it('is-england without northing should throw "No point provided"', async () => {
-    await expect(
-      isEnglandService.isEnglandService(388244, undefined)
-    ).rejects.toThrow('No point provided')
-  })
+    it('should throw "No polygon provided" when polygon is undefined', async () => {
+      await expect(
+        isEnglandService.isPolygonInEngland(undefined)
+      ).rejects.toThrow('No polygon provided')
+    })
 
-  it('is-england with northing and easting should call esriFeatureRequest"', async () => {
-    const point = { northing: 388244, easting: 388244 }
-    const response = await isEnglandService.isEnglandService(point.easting, point.northing)
-    expect(response).toEqual(true)
+    it('should throw "No polygon provided" when polygon is null', async () => {
+      await expect(
+        isEnglandService.isPolygonInEngland(null)
+      ).rejects.toThrow('No polygon provided')
+    })
+
+    it('should return true when esriFeatureRequest returns a non-empty array', async () => {
+      const response = await isEnglandService.isPolygonInEngland(polygon)
+      expect(response).toEqual(true)
+    })
+
+    it('should return false when esriFeatureRequest returns an empty array', async () => {
+      const { mockEsriRequest: mockEmpty } = require('./__mocks__/agol')
+      mockEmpty([])
+      const response = await isEnglandService.isPolygonInEngland(polygon)
+      expect(response).toEqual(false)
+    })
   })
 })
