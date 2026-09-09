fa21d9e44f4118a99646073554cc7b228a2370f3
Mark Fee <mark.fee@defra.gov.uk>
2026-09-09 15:18:56 +0100

    FCRM-7018 removed the lazy load of the plugin

--- client/js/map/plugins/opacity-slider/src/index.js
+++ client/js/map/plugins/opacity-slider/src/index.js
@@ -1,12 +1,14 @@
 import './flood-menu.scss'
-
-export default function createPlugin (options = {}) {
+import { manifest } from './manifest.js'
+const createPlugin = (options = {}) => {
   return {
     ...options,
     id: 'floodMenu',
     load: async () => {
-      const module = (await import(/* webpackChunkName: "flood-menu-plugin" */ './manifest.js')).manifest
+      const module = manifest
       return module
     }
   }
 }
+
+export default createPlugin
