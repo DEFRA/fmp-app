b41625d7aa074a11bec38bfd58a1c7c02a90e3e8
Michael Steinacher <steinacherm@hotmail.co.uk>
2026-09-22 12:33:25 +0100

    removed unnecesary esri geo proxy

--- fmp-proxy/server/plugins/router.js
+++ fmp-proxy/server/plugins/router.js
@@ -2,7 +2,6 @@ const routes = [].concat(
   require('../routes/health-check'),
   require('../routes/esri-proxy'),
   require('../routes/esri-tiles-proxy'),
-  require('../routes/esri-geocode-proxy'),
   require('../routes/os-place-lookup'),
   require('../routes/os-basemap')
 )
